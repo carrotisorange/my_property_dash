5e6306b4dcc6dd058868f1d9c6e590121f24a2ec
carrotisorange <landleydgreat@gmail.com>
2020-06-02 12:02:22 +0800

    show all the tenants.

--- resources/views/billings/show-payments.blade.php
+++ resources/views/billings/show-payments.blade.php
@@ -14,11 +14,6 @@
         </tr>
         <?php $ctr = 1; ?> 
         @foreach ($payments as $item)
-         @if($payments->count() <= 0)
-        <tr>
-            <td colspan="6" class="text-center">No payments found!</td>
-        </tr>
-        @else
         <tr>
             <th class="text-center">{{ $ctr++ }}</th>
             <td>{{ Carbon\Carbon::parse($item->payment_created)->format('M d Y') }}</td>
@@ -33,7 +28,6 @@
                 </form>
             </td> --}}
         </tr>
-        @endif
         @endforeach
     </table>
 </div>
