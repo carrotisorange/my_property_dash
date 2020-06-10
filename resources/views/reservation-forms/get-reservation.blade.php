986af8fa9dd291798e92f73a837222a3a7a77f15
carrotisorange <landleydgreat@gmail.com>
2020-06-10 10:01:38 +0800

    fxed the reservation form.

--- resources/views/reservation-forms/get-reservation.blade.php
+++ resources/views/reservation-forms/get-reservation.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app')
 @section('title', $unit->building.' '.$unit->unit_no)
 @section('content')
-<div class="container-fluid">
+<div class="container">
     <div class="row">
-       <div class="col-md-6">
-           <h5>Tenant Information</h5>
+       <div class="col-md-12">
+           <p>Tenant information</p>
         <table class="table table-striped">
             <tr>
-                <td>Reservation Date:</td>
+                <th>Date of reservation</th>
                 <td>{{ $tenant->created_at }}</td>
             </tr>
                 <tr>
-                    <td>Full Name:</td>
+                    <th>Full Name</th>
                     <td>{{ $tenant->first_name.' '.$tenant->middle_name.' '.$tenant->last_name }} 
                         @if($tenant->tenant_status === 'active')
                             <a class="badge badge-primary">{{ $tenant->tenant_status }}</a>
@@ -23,127 +23,25 @@
                     </td>
                 </tr>
                 <tr>
-                    <td>Property:</td>
+                    <th>Property</th>
                     <td>{{ $unit->unit_property }}</td>
                 </tr>
                 <tr>
-                    <td>Unit No:</td>
+                    <th>Unit No</th>
                     <td>{{ $unit->building.' '.$unit->unit_no }}</td>
                 </tr>
                 <tr>
-                    <td>Monthly Rent:</td>
+                    <th>Monthly Rent</th>
                     <td>{{ number_format($tenant->tenant_monthly_rent, 2) }}</td>
                 </tr>
                 <tr>
-                    <td>Contract Duration:</td>
+                    <th>Contract Duration</th>
                     <td>{{ Carbon\Carbon::parse($tenant->movein_date)->format('M d Y').'-'.Carbon\Carbon::parse($tenant->moveout_date)->format('M d Y') }} <a class="badge badge-primary">{{ $tenant->has_extended }}</a>
                     </td>
                 </tr>
-                <tr>
-                    <td>Note:</td>
-                    <td>
-                        {{ $tenant->tenants_note }}
-                    </td>
-                </tr>
             </table>
-            <table class="table table-striped">
-                <tr>
-                    <th colspan="2" class="text-center">Other Information</th>
-    
-                </tr>
-                <tr>
-                    <td>Gender</td>
-                    <td>{{ $tenant->gender }}</td>
-                </tr>
-                <tr>
-                    <td>Birthdate</th>
-                    <td>{{ Carbon\Carbon::parse($tenant->birthdate)->format('M d Y') }}</td>
-                </tr>
-                <tr>
-                    <td>Civil Status</td>
-                    <td>{{ $tenant->civil_status }}</td>
-                </tr>
-                <tr>
-                    <td>ID/ID Number</td>
-                    <td>{{ $tenant->id_number }}</td>
-                </tr>
-                <tr>
-                    <td>Address</td>
-                    <td>{{ $tenant->barangay.', '.$tenant->city.', '.$tenant->province.', '.$tenant->country.', '.$tenant->zip_code }}</td>
-                </tr>
-              
-                <tr>
-                    <td>Contact No</td>
-                    <td>{{ $tenant->contact_no }}</td>
-                </tr>
-                <tr>
-                    <td>Email Address</td>
-                    <td>{{ $tenant->email_address }}</td>
-                </tr>
-                <tr>
-                    <th colspan="2">Person to contact in case of emergency</th>
-    
-                </tr>
-                <tr>
-                    <td>Name</td>
-                    <td>{{ $tenant->guardian }}</td>
-                </tr>
-                <tr>
-                    <td>Relationship with the tenant</td>
-                    <td>{{ $tenant->guardian_relationship }}</td>
-                </tr>
-                <tr>
-                    <td>Contact No</td>
-                    <td>{{ $tenant->guardian_contact_no }}</td>
-                </tr>
-            
-                <tr>
-                    <th>Education Background</th>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>High School</td>
-                    <td>{{ $tenant->high_school.', '.$tenant->high_school_address }}</td>
-                </tr>
-                <tr>
-                    <td>College/University</td>
-                    <td>{{ $tenant->college_school.', '.$tenant->college_school_address }}</td>
-                </tr>
-                <tr>
-                    <td>Course/Year</td>
-                    <td>{{ $tenant->course.', '.$tenant->year_level }}</td>
-                </tr>
-              
-                <tr>
-                    <th colspan="2">Employment Information</th>
-    
-                </tr>
-                <tr>
-                    <td>Employer</td>
-                    <td>{{ $tenant->employer}}</td>
-                </tr>
-                <tr>
-                    <td>Address</td>
-                    <td>{{ $tenant->employer_address }}</td>
-                </tr>
-                <tr>
-                    <td>Contact No</td>
-                    <td>{{ $tenant->employer_contact_no }}</td>
-                </tr>
-    
-                <tr>
-                    <td>Job description</td>
-                    <td>{{ $tenant->job }}</td>
-                </tr>
-                <tr>
-                    <td>Years of employment</td>
-                    <td>{{ $tenant->years_of_employment }}</td>
-                </tr>
-            </table>
-       </div>
-
-       <div class="col-md-6">
-           <h5>Amount to be paid before moving-in...</h5>
+       
+           <p>Breakdowns of the amount to be paid moving-in</p>
         <table class="table">
             <tr>
                 <th class="text-center">#</th>
@@ -166,8 +64,16 @@
                 <td></td>
                 <th>{{ number_format($billings->sum('billing_amt'),2) }}</th>
             </tr>
+            
         </table>
-       </div>
+    </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-md-12">
+          
+            <h5 class="text-center"> {{ $tenant->tenants_note }}</h5>
+        </div>
     </div>
 </div>
 @endsection
--- resources/views/reservation-forms/show-reservation-form.blade.php
+++ resources/views/reservation-forms/show-reservation-form.blade.php
@@ -11,25 +11,26 @@
         <input form="reservationForm" type="hidden" value="{{ $unit->unit_property }}" name="unit_property"> 
         <div class="row">
             <div class="col">
-                <label for="recipient-name" class="col-form-label">First Name:</label>
+                <small>First Name:</small>  
                 <input form="reservationForm" type="text" class="form-control" name="first_name" id="first_name" value="" required>
             </div>
             <div class="col">
-                <label for="recipient-name" class="col-form-label">Middle Name:</label>
+                <small>Middle Name:</small>  
                 <input form="reservationForm" type="text" class="form-control" name="middle_name" id="middle_name" value="">
             </div>
             <div class="col">
-                <label for="recipient-name" class="col-form-label">Last Name:</label>
+                <small>Last Name:</small>  
                 <input form="reservationForm" type="text" class="form-control" name="last_name" id="last_name" value="" required>
             </div>
             </div>
+            <br>
         <div class="row">
             <div class="col">
-                <label for="recipient-name" class="col-form-label">Birthdate:</label>
+                <small>Birthdate:</small>  
                 <input form="reservationForm" type="date" class="form-control" name="birthdate" id="birthdate" value="">
             </div>
             <div class="col">
-                <label for="recipient-name" class="col-form-label">Gender:</label>
+                <small>Gender:</small>  
                 <select form="reservationForm"  id="gender" name="gender" class="form-control" required >        
                     <option value="" selected>Please select one</option>
                     <option value="male">male</option>
@@ -49,21 +50,21 @@
                 <input form="reservationForm" type="text" class="form-control" name="id_number" id="id_number" value="">
             </div> --}}
         </div>
-        
+        <br>
         <div class="row">
             <div class="col">
-              <label for="recipient-name" class="col-form-label">Contact Number:</label>
+                <small>Mobile:</small>  
               <input form="reservationForm" type="text" class="form-control" name="contact_no" id="contact_no" value="" required >
             </div>
             <div class="col">
-              <label for="recipient-name" class="col-form-label">Email Address:</label>
+                <small>Email:</small>  
               <input form="reservationForm" type="email" class="form-control" name="email_address" id="email_address" value="">
             </div>
         </div>
 
         <div class="row">
             <div class="col">
-                <label for="recipient-name" class="col-form-label">House No/Barangay:</label>
+                <small>House No/Barangay:</small>  
                 <input form="reservationForm" type="text" class="form-control" name="barangay" id="barangay" value="">
             </div>
             <div class="col">
@@ -102,7 +103,7 @@
             </div>
        </div>
        <br> --}}
-       <h5>Educational Background (for student) </h5>
+       {{-- <h5>Educational Background (for student) </h5>
     <hr>
     <div class="row">
         <div class="col">
@@ -170,51 +171,50 @@
             <input form="reservationForm" type="text" class="form-control" name="employer_address" id="employer_address" value="">
         </div>
     </div>
-    <br>
+    <br> --}}
     <h5>Contract Duration</h5>
     <hr>
     <div class="row">
-        <div class="col">
+        <div class="col-md-4">
           <label for="recipient-name" class="col-form-label">Move In Date:</label>
           <input form="reservationForm" type="date" class="form-control" name="movein_date" id="movein_date" value={{date('Y-m-d')}} required>
-        </div>
-        <div class="col">
-          <label for="recipient-name" class="col-form-label">Move Out Date:</label>
-          <input onkeyup="duration()" form="reservationForm" type="date" class="form-control" name="moveout_date" value="" required>
         </div>
         {{-- <div class="col"> --}}
             {{-- <label for="recipient-name" class="col-form-label">Monthly Rent:</label> --}}
             <input form="reservationForm" type="hidden" class="form-control" name="tenant_monthly_rent" id="tenant_monthly_rent" value="{{ $unit->monthly_rent }}" required readonly>
         {{-- </div> --}}
       </div>
-      <br><br>
+      <br>
+      <div class="row">
+        <div class="col-md-4">
+            <label for="recipient-name" class="col-form-label">Move Out Date:</label>
+            <input onkeyup="duration()" form="reservationForm" type="date" class="form-control" name="moveout_date" value="" required>
+          </div>
+      </div>
+      <br>
       <h5>Payment Requirements</h5>
-    <h5><p class="text-right" id="total_bills">Total: </p></h5>
     <div class="row">
         <div class="col">
-             <table class="table" id="tab_logic">
-                 <tr>
-                     <th class="text-center">#</th>
-                     <th>Description</th>
-                     <th>Amount</th>
-                 </tr>
+             <table class="table table-borderless" id="tab_logic">
                      <input form="reservationForm" type="hidden" id="no_of_items" name="no_of_items" value="3">
                  <tr id='addr0'>
-                     <th class="text-center">1</th>
+                     
                      <td><input form="reservationForm"  type="text" name='desc0' id='desc0' class="form-control" value="Security Deposit (Rent)" readonly/></td>
                      <td><input oninput="this.value = Math.abs(this.value)" form="reservationForm"  onkeyup="computeTotal()" type="number" name='amt0' id='amt0' class="form-control" value="{{ $unit->monthly_rent }}" readonly/></td>
                  </tr>
                  <tr id='addr1'>
-                    <th class="text-center">2</th>
+                   
                     <td><input form="reservationForm"  type="text" name='desc1' id='desc1' class="form-control" value="Security Deposit (Utilities)" readonly/></td>
                     <td><input oninput="this.value = Math.abs(this.value)" form="reservationForm"  onkeyup="computeTotal()" type="number" name='amt1' id='amt1' class="form-control" value="2000" readonly/></td>
                 </tr>
                  <tr>
-                     <th class="text-center">3</th>
+                     
                     <td><input form="reservationForm"  type="text" name='desc2' id='desc2' class="form-control" value="Advance Rent" readonly/></td>
                     <td><input oninput="this.value = Math.abs(this.value)" form="reservationForm"  onkeyup="computeTotal()" type="number" name='amt2' id='amt2' class="form-control" value="{{ $unit->monthly_rent }}" readonly/></td>
                  </tr>
-                 <tr id='addr2'></tr>
+                 <tr id='addr2'>
+                    <h5><p class="text-right" id="total_bills">Total: </p></h5>
+                 </tr>
              </table>      
         </div>
         
