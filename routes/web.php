47d1b203f98405b39ed610eededa1ba9bb7f4b02
carrotisorange <landleydgreat@gmail.com>
2020-06-16 14:50:16 +0800

    added the occupancy rate in the dashboard.

--- routes/web.php
+++ routes/web.php
@@ -750,12 +750,12 @@ Route::get('/', function(Request $request){
     $movein_rate->displaylegend(false);
     $movein_rate->labels([Carbon::now()->subMonth(5)->format('M Y'),Carbon::now()->subMonth(4)->format('M Y'),Carbon::now()->subMonth(3)->format('M Y'),Carbon::now()->subMonths(2)->format('M Y'),Carbon::now()->subMonth()->format('M Y'),Carbon::now()->format('M Y')]);
     $movein_rate->dataset('', 'line', [
-                                                                number_format((($movein_rate_1 + $active_tenants->count())-($movein_rate_2 + $movein_rate_3 + $movein_rate_4 + $movein_rate_5 + $movein_rate_6))/$leasing_units->count() * -100,2),
-                                                                number_format((($movein_rate_2 + $active_tenants->count())-($movein_rate_1 + $movein_rate_3 + $movein_rate_4 + $movein_rate_5 + $movein_rate_6 + $active_tenants->count()))/$leasing_units->count() * -100,2),
-                                                                number_format((($movein_rate_3 + $active_tenants->count())-($movein_rate_1 + $movein_rate_2 + $movein_rate_4 + $movein_rate_5 + $movein_rate_6 + $active_tenants->count()))/$leasing_units->count() * -100,2),
-                                                                number_format((($movein_rate_4 + $active_tenants->count())-($movein_rate_1 + $movein_rate_2 + $movein_rate_3 + $movein_rate_5 + $movein_rate_6 + $active_tenants->count()))/$leasing_units->count() * -100,2),
-                                                                number_format((($movein_rate_5 + $active_tenants->count())-($movein_rate_1 + $movein_rate_2 + $movein_rate_3 + $movein_rate_4 + $movein_rate_6 + $active_tenants->count()))/$leasing_units->count() * -100,2),
-                                                                number_format((($movein_rate_6 + $active_tenants->count())-($movein_rate_1 + $movein_rate_2 + $movein_rate_3 + $movein_rate_4 + $movein_rate_5 + $active_tenants->count()))/$leasing_units->count() * -100,2),
+                                                                number_format((($active_tenants->count() - $movein_rate_2 + $movein_rate_3 + $movein_rate_4 + $movein_rate_5 + $movein_rate_6)+($movein_rate_1))/$leasing_units->count() * 100,2),
+                                                                number_format((($active_tenants->count() - $movein_rate_1 + $movein_rate_3 + $movein_rate_4 + $movein_rate_5 + $movein_rate_6)+($movein_rate_2))/$leasing_units->count() * 100,2),
+                                                                number_format((($active_tenants->count() - $movein_rate_1 + $movein_rate_2 + $movein_rate_4 + $movein_rate_5 + $movein_rate_6)+($movein_rate_3))/$leasing_units->count() * 100,2),
+                                                                number_format((($active_tenants->count() - $movein_rate_1 + $movein_rate_2 + $movein_rate_3 + $movein_rate_5 + $movein_rate_6)+($movein_rate_4))/$leasing_units->count() * 100,2),
+                                                                number_format((($active_tenants->count() - $movein_rate_1 + $movein_rate_2 + $movein_rate_3 + $movein_rate_4 + $movein_rate_6)+($movein_rate_5))/$leasing_units->count() * 100,2),
+                                                                number_format((($active_tenants->count() - $movein_rate_1 + $movein_rate_2 + $movein_rate_3 + $movein_rate_4 + $movein_rate_5)+($movein_rate_6))/$leasing_units->count() * 100,2),
                                                             ])
     ->color("rgb(0, 0, 0)")
     ->backgroundcolor("rgb(169, 169, 169)")
