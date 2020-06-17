6f31b2b8e55b6573974f66d7e26ffbbadb9831af
carrotisorange <landleydgreat@gmail.com>
2020-06-17 11:17:34 +0800

    show only the leasing units in the units.

--- resources/views/tenants.blade.php
+++ resources/views/tenants.blade.php
@@ -19,7 +19,7 @@
                      <th>unit no</th>
                      <th>contact no</th>
                      <th>monthly rent</th>
-                     <th>contract expires in</th>    
+                     <th>contract</th>    
                 </tr>
                 </thead>
                 <?php
@@ -47,7 +47,8 @@
                     <td>{{ $item->building.' '.$item->unit_no }}</td>
                     <td>{{ $item->contact_no }}</td>
                     <td>{{ number_format($item->tenant_monthly_rent,2) }}</td>
-                    <td title="months before move-out">{{ number_format(Carbon\Carbon::now()->diffInDays(Carbon\Carbon::parse($item->moveout_date), false)/30,1) }} mon</td>
+                    <td>{{ Carbon\Carbon::parse($item->movein_date)->format('M d Y').' - '.Carbon\Carbon::parse($item->moveout_date)->format('M d Y') }}</td>
+                    {{-- <td title="months before move-out">{{ number_format(Carbon\Carbon::now()->diffInDays(Carbon\Carbon::parse($item->moveout_date), false)/30,1) }} mon</td> --}}
                 </tr>
                 @endforeach
                 </tbody>
