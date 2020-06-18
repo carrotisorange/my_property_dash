73506821d5663c04e52dd4a092c838bb2993f6ba
carrotisorange <landleydgreat@gmail.com>
2020-06-18 11:36:56 +0800

    change the name of the application.

--- app/Http/Controllers/UnitsController.php
+++ app/Http/Controllers/UnitsController.php
@@ -203,8 +203,17 @@ class UnitsController extends Controller
      */
     public function update(Request $request, $id)
     {
-       DB::table('units')->where('unit_id', $id)->delete();
-
+        DB::table('units')->where('unit_id', $id)
+            ->update([
+                'unit_no' => $request->unit_no,
+                'floor_no' => $request->floor_no,
+                'beds' => $request->beds,
+                'status' => $request->status,
+                'building' => $request->building,
+                'type_of_units' => $request->type_of_units,
+                'monthly_rent' => $request->monthly_rent
+            ]);
+        
         return back()->with('success', 'Unit information has been successfully updated!');
     }
 
