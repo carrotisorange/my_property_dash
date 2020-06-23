632923ce9423bc103cf798b0aeb7e2e98deb1233
carrotisorange <landleydgreat@gmail.com>
2020-06-23 22:33:14 +0800

    fixed some bugs.

--- resources/views/leasing.blade.php
+++ resources/views/leasing.blade.php
@@ -352,8 +352,8 @@
 
           @if($leasing_units->count() <= 0 )
           <div class="text-center">
-            <div class="error mx-auto" data-text="">No units/rooms found</div>
-            {{-- <p class="lead text-gray-800 mb-5">units/rooms found</p> --}}
+            <div class="error mx-auto" data-text=""></div>
+            <p class="lead text-gray-800 mb-5">No units/rooms found!</p>
             {{-- <p class="text-gray-500 mb-0">No units/rooms found...</p> --}}
             <a href="/">&larr; Add your first unit/room</a>
           </div>
