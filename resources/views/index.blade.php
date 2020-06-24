01803c54a71636d9da14c1de3b3f30937ff5dd08
carrotisorange <landleydgreat@gmail.com>
2020-06-24 19:34:16 +0800

    asdasd

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -45,7 +45,7 @@
           <li><a href="#portfolio">Portfolio</a></li>
           <li><a href="#team">Team</a></li>
           <li><a href="#pricing">Pricing</a></li>
-          <li><a href="">Foods</a></li>
+          <li><a href="/foods">Foods</a></li>
           <li><a href="#contact">Contact</a></li>
           
 
