fa499d82861b9df1f438b1bb749425b31d4d199e
carrotisorange <landleydgreat@gmail.com>
2020-06-24 23:01:18 +0800

    fixed the logo.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -42,7 +42,7 @@
 
       <h1 class="logo mr-auto"><a href="/">The Property Manager</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
-       <a href="index.html" class="logo mr-auto"><img src="{{asset('index/assets/img/logo.png')}}" alt="" class="img-fluid"></a>
+       <a href="/" class="logo mr-auto"><img src="{{asset('index/assets/img/logo.png')}}" alt="" class="img-fluid"></a>
 
       <nav class="nav-menu d-none d-lg-block">
         <ul>
