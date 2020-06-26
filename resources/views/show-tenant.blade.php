d87d513dbbcb6fb81e975d0a867f220d44a89038
carrotisorange <landleydgreat@gmail.com>
2020-06-26 14:40:25 +0800

    fixed bugs.

--- resources/views/show-tenant.blade.php
+++ resources/views/show-tenant.blade.php
@@ -358,7 +358,7 @@
         <div class="container-fluid">
           @foreach (['danger', 'warning', 'success', 'info'] as $key)
           @if(Session::has($key))
-         <p class="alert alert-{{ $key }}"> <i class="fas fa-check-circle"></i>{{ Session::get($key) }}</p>
+         <p class="alert alert-{{ $key }}"> <i class="fas fa-check-circle"></i> {{ Session::get($key) }}</p>
           @endif
           @endforeach
             <h5 style="text-align:left;">
