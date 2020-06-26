4847c4b31bf11d19eefed9e8e8d9247aacf870a3
carrotisorange <landleydgreat@gmail.com>
2020-06-26 11:59:29 +0800

    improve dashboard.

--- resources/views/treasury/show-payment.blade.php
+++ resources/views/treasury/show-payment.blade.php
@@ -355,6 +355,11 @@
         </nav>
         <!-- End of Topbar -->
         <div class="container-fluid">
+
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Payment Details</h1>
+            {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> --}}
+          </div>
         <!-- 404 Error Text -->
         <div class="table-responsive">
             @foreach ($payment as $item)
