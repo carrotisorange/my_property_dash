7c4486f345d9e1cf6adcc84c309e5b855e71b59c
carrotisorange <landleydgreat@gmail.com>
2020-07-07 15:31:51 +0800

    asdasd

--- resources/views/admin/show-investor.blade.php
+++ resources/views/admin/show-investor.blade.php
@@ -319,11 +319,13 @@
         </nav>
         <!-- End of Topbar -->
         <div class="container-fluid">
+
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Owners</h1>
+            <button type="button" class="class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"" data-toggle="modal" data-target="#editTenant" data-whatever="@mdo"><i class="fas fa-edit fa-sm text-white-50"></i>Edit</button>  
+          </div>
         <!-- 404 Error Text -->
         <div class="table-responsive">
-            <p style="float:right;">
-                <button type="button" title="edit this unit information." class="btn btn-primary" data-toggle="modal" data-target="#editTenant" data-whatever="@mdo"><i class="fas fa-edit"></i>EDIT</button> 
-            </p>
             <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                <tr>
                    <th>Unit Owner</th>
