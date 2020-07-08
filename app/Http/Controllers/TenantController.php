5a4f18a91e4f626837ffe1516dff64e5ab2b65bb
carrotisorange <landleydgreat@gmail.com>
2020-07-08 13:52:14 +0800

    added the auto-paid bill during movein.

--- app/Http/Controllers/TenantController.php
+++ app/Http/Controllers/TenantController.php
@@ -651,13 +651,9 @@ class TenantController extends Controller
      */
     public function destroy($tenant_id)
     {
-        // DB::table('payments')->where('payment_tenant_id', $tenant_id)->delete();
-        // DB::table('billings')->where('billing_tenant_id', $tenant_id)->delete();
-        // DB::table('tenants')->where('tenant_id', $tenant_id)->delete();
-
-        DB::table('billings')->where('billing_tenant_id', $tenant_id)->update([
-            'billing_status' => 'paid'
-        ]);
+        DB::table('payments')->where('payment_tenant_id', $tenant_id)->delete();
+        DB::table('billings')->where('billing_tenant_id', $tenant_id)->delete();
+        DB::table('tenants')->where('tenant_id', $tenant_id)->delete();
 
         return back()->with('success', 'Tenant has been successfully deleted!');
     }
