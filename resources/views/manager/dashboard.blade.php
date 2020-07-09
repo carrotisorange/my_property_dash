faaf05e0548ff2787ed9bcad0239b80b4b951d84
carrotisorange <landleydgreat@gmail.com>
2020-07-09 15:56:13 +0800

    rearrange the dashboard.

--- resources/views/manager/dashboard.blade.php
+++ resources/views/manager/dashboard.blade.php
@@ -426,7 +426,7 @@
                                <th>TENANT</th>
                                {{-- <th>CONTACT</th>
                                <th>UNIT/ROOM </th> --}}
-                               <th colspan="3"></th>
+                               <th>STATUS</th>
                            </tr>
                            </thead>
                            <tbody>
@@ -464,6 +464,52 @@
                    </div>
            
                        </div>
+
+                        <!-- Pie Chart -->
+                  <div class="col-xl-6 col-lg-5">
+                    <div class="card shadow mb-3">
+                      <!-- Card Header - Dropdown -->
+                      <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                        <h6 class="m-0 font-weight-bold text-primary">DELINQUENTS</h6>
+                        <div class="dropdown no-arrow">
+                          <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                          </a>
+                          {{-- <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                            <div class="dropdown-header">Dropdown Header:</div>
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                          </div> --}}
+                        </div>
+                      </div>
+                      <!-- Card Body -->
+                      <div class="card-body">
+                        <div class="table-responsive">
+                          <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                              <tr>
+                                <th>TENANT</th>
+                                <th>UNIT/ROOM</th>
+                                <th>AMOUNT</th>
+                               
+                            </tr>
+                            </thead>
+                            <tbody>
+                              @foreach($delinquent_accounts as $item)
+                              <tr>
+                                <td><a href="{{ route('show-tenant',['unit_id' => $item->unit_id, 'tenant_id'=>$item->tenant_id]) }}">{{ $item->first_name.' '.$item->last_name }}</a></td>
+                                <td>{{ $item->building.' '.$item->unit_no }}</td>
+                                <td>{{ number_format($item->total_bills,2) }}</td>
+                              </tr>
+                              @endforeach
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
                 <div class="row">
       
@@ -567,51 +613,7 @@
                     </div>
                   </div>
       
-                  <!-- Pie Chart -->
-                  <div class="col-xl-6 col-lg-5">
-                    <div class="card shadow mb-3">
-                      <!-- Card Header - Dropdown -->
-                      <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                        <h6 class="m-0 font-weight-bold text-primary">DELINQUENTS</h6>
-                        <div class="dropdown no-arrow">
-                          <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                          </a>
-                          {{-- <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                            <div class="dropdown-header">Dropdown Header:</div>
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                          </div> --}}
-                        </div>
-                      </div>
-                      <!-- Card Body -->
-                      <div class="card-body">
-                        <div class="table-responsive">
-                          <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                            <thead>
-                              <tr>
-                                <th>TENANT</th>
-                                <th>UNIT/ROOM</th>
-                                <th>AMOUNT</th>
-                               
-                            </tr>
-                            </thead>
-                            <tbody>
-                              @foreach($delinquent_accounts as $item)
-                              <tr>
-                                <td><a href="{{ route('show-tenant',['unit_id' => $item->unit_id, 'tenant_id'=>$item->tenant_id]) }}">{{ $item->first_name.' '.$item->last_name }}</a></td>
-                                <td>{{ $item->building.' '.$item->unit_no }}</td>
-                                <td>{{ number_format($item->total_bills,2) }}</td>
-                              </tr>
-                              @endforeach
-                            </tbody>
-                          </table>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                 
                 </div>
       
                 <!-- Content Row -->
