ab4ef792b357cb48ba80801831fb9e1a185dbf26
carrotisorange <landleydgreat@gmail.com>
2020-07-09 11:13:39 +0800

    added delete button for user.

--- routes/web.php
+++ routes/web.php
@@ -1320,16 +1320,9 @@ Route::get('/home', function(){
 
     if(auth()->user()->status === 'registered' && (auth()->user()->user_type === 'manager' || auth()->user()->user_type === 'admin') ){
         $units = DB::table('units')
-        ->where('unit_property', Auth::user()->property)
-        ->where('status','!=', 'pulled out')
-        ->count();
-
-        $units_per_building = DB::table('units')
-            ->select('building', 'status', DB::raw('count(*) as count'))
             ->where('unit_property', Auth::user()->property)
-            ->groupBy('building')
             ->where('status','!=', 'pulled out')
-            ->get('building', 'status','count');   
+            ->count();
 
         $units_vacant= DB::table('units')
             ->where('unit_property', Auth::user()->property)
@@ -1351,20 +1344,26 @@ Route::get('/home', function(){
 
         $units_reserved= DB::table('units')
             ->where('unit_property', Auth::user()->property)
-            
             ->where('status','reserved')
             ->orderBy('building')
             ->orderBy('floor_no')
             ->orderBy('unit_no')
             ->get();
 
+        $units_per_building = DB::table('units')
+            ->select('building', 'status', DB::raw('count(*) as count'))
+            ->where('unit_property', Auth::user()->property)
+            ->groupBy('building')
+            ->where('status','!=', 'pulled out')
+            ->get('building', 'status','count');   
+
+            
         $units_per_status = DB::table('units')
             ->select('status',DB::raw('count(*) as count'))
             ->where('unit_property', Auth::user()->property)
             ->where('status','!=', 'pulled out')
             ->groupBy('status')
             ->get();
-        
         
         return view('admin.home',compact('units','units_per_building','units','units_per_status'));
         }else{
