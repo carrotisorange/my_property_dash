2de36ec743b2ced39af9033a181f9d195e06689f
carrotisorange <landleydgreat@gmail.com>
2020-07-09 12:55:44 +0800

    added the login history of users.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -42,7 +42,7 @@ class LoginController extends Controller
         function authenticated(Request $request, $user)
         {
             $user->update([
-                'last_login_at' => Carbon::now()->toDateTimeString(),
+                'last_login_at' => Carbon::now(),
                 'last_login_ip' => $request->getClientIp()
             ]);
         }
