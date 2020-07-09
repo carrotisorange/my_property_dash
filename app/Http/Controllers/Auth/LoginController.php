03dbaadf575cc6103ad3409fdeccd119a78bd1ba
carrotisorange <landleydgreat@gmail.com>
2020-07-09 12:05:34 +0800

    added the login history of users.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
-use Carbon;
+use Carbon\Carbon;
 
 use Illuminate\Http\Request;
 class LoginController extends Controller
