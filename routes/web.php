14c91c92c1c8f34dc259d3e0626d19adc9083d8d
carrotisorange <landleydgreat@gmail.com>
2020-07-15 17:12:45 +0800

    asdasd

--- routes/web.php
+++ routes/web.php
@@ -1303,7 +1303,7 @@ Route::get('/', function(Request $request){
             ); 
     }
 
-    })->middleware(['auth']);
+    });
 
 //routes for units
 Route::get('units/{unit_id}', 'UnitsController@show')->middleware('auth');
