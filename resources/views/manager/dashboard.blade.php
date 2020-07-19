6ca2fa187866f4cd81d8f4698303824a0a40a6a4
carrotisorange <landleydgreat@gmail.com>
2020-07-20 02:52:31 +0800

    edit feadures for commercial complex.

--- resources/views/manager/dashboard.blade.php
+++ resources/views/manager/dashboard.blade.php
@@ -415,7 +415,7 @@
                 <div class="row">
       
                   <!-- Earnings (Monthly) Card Example -->
-                  <div class="col-xl-3 col-md-6 mb-4">
+                  <div class="col-xl-4 col-md-6 mb-4">
                     <div class="card border-left-primary shadow h-100 py-2">
                       <div class="card-body">
                         <div class="row no-gutters align-items-center">
@@ -437,7 +437,7 @@
                   </div>
       
                   <!-- Earnings (Monthly) Card Example -->
-                  <div class="col-xl-3 col-md-6 mb-4">
+                  <div class="col-xl-4 col-md-6 mb-4">
                     <div class="card border-left-success shadow h-100 py-2">
                       <div class="card-body">
                         <div class="row no-gutters align-items-center">
