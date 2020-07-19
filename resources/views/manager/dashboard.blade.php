d6c336e874214cefc6a91d77e2257fb3f68fe952
carrotisorange <landleydgreat@gmail.com>
2020-07-20 02:51:54 +0800

    edit feadures for commercial complex.

--- resources/views/manager/dashboard.blade.php
+++ resources/views/manager/dashboard.blade.php
@@ -324,7 +324,10 @@
                   {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> --}}
                 </div>
       
-                <!-- Content Row -->
+             
+
+                @if(Auth::user()->property_type !== 'Commercial Complex')
+   <!-- Content Row -->
                 <div class="row">
       
                   <!-- Earnings (Monthly) Card Example -->
@@ -368,7 +371,7 @@
                     </div>
                   </div>
       
-                  @if(Auth::user()->property_type !== 'Commercial Complex')
+                
                   <!-- Earnings (Monthly) Card Example -->
                   <div class="col-xl-3 col-md-6 mb-4">
                     <div class="card border-left-warning shadow h-100 py-2">
@@ -387,7 +390,7 @@
                       </div>
                     </div>
                   </div>
-                  @endif
+                  
                   <!-- Pending Requests Card Example -->
                   <div class="col-xl-3 col-md-6 mb-4">
                     <div class="card border-left-danger shadow h-100 py-2">
@@ -406,6 +409,72 @@
                     </div>
                   </div>
                 </div>
+
+                @else
+   <!-- Content Row -->
+                <div class="row">
+      
+                  <!-- Earnings (Monthly) Card Example -->
+                  <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                      <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                          <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">UNITS/ROOMS </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $units->count() }}</div>
+                            
+                            <small>O ({{ $units_occupied->count() }})</small>
+                            <small>V ({{ $units_vacant->count() }})</small>
+                            <small>R ({{ $units_reserved->count() }})</small>
+                            
+                          </div>
+                          <div class="col-auto">
+                              <i class="fas fa-home fa-2x text-gray-300"></i>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+      
+                  <!-- Earnings (Monthly) Card Example -->
+                  <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-success shadow h-100 py-2">
+                      <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                          <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">ACTIVE TENANTS </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $active_tenants->count() }}</div>
+                            <small>P ({{ $pending_tenants->count() }})</small>
+                            
+                          </div>
+                          <div class="col-auto">
+                            <i class="fas fa-user-check fa-2x text-gray-300"></i>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  
+                  <!-- Pending Requests Card Example -->
+                  <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-danger shadow h-100 py-2">
+                      <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                          <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">PENDING JOB ORDERS</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">0</div>
+                            <small>|</small>
+                          </div>
+                          <div class="col-auto">
+                            <i class="fas fa-tools fa-2x text-gray-300"></i>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                @endif
       
                 <!-- Content Row -->
                 <div class="row">
