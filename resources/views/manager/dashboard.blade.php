fcf4ab3ed9e9da441f92e957f65d8a6612bc73a3
carrotisorange <landleydgreat@gmail.com>
2020-07-20 11:29:59 +0800

    fixed the price.

--- resources/views/manager/dashboard.blade.php
+++ resources/views/manager/dashboard.blade.php
@@ -326,7 +326,7 @@
       
              
 
-                @if(Auth::user()->property_type !== 'Commercial Complex')
+                @if(Auth::user()->property_type !== 'Commercial Complex' || Auth::user()->property_ownership === 'Multiple Ownership')
    <!-- Content Row -->
                 <div class="row">
       
