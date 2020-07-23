39bc61f0e0e2c67539170bf086806b3eb0b1ec41
carrotisorange <landleydgreat@gmail.com>
2020-07-23 23:06:28 +0800

    changed monthly rent to rent.

--- resources/views/billing/collections.blade.php
+++ resources/views/billing/collections.blade.php
@@ -334,7 +334,7 @@
                           <th>BILL NO</th>
                           <th>TENANT</th>
                           <th>UNIT/ROOM</th>
-                          
+                          <th>desc</th>
                           <th>AMOUNT</th>
                           <th></th>
                       </tr>
@@ -346,7 +346,7 @@
                           
                           <td>{{ $item->first_name.' '.$item->last_name }}</td>
                           <td>{{ $item->building.' '.$item->unit_no }}</td>
-                          
+                          <th>{{ $item->billing_desc }}</th>
                           <td>{{ number_format($item->total,2) }}</td>
                           <td>
                             <a title="export pdf" target="_blank" href="/units/{{ $item->unit_id }}/tenants/{{ $item->tenant_id }}/payments/{{ $item->payment_id }}/dates/{{$item->payment_created}}/export" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i></a>
