6e45cc306a652131d057535f2fe561bce0255958
carrotisorange <landleydgreat@gmail.com>
2020-07-26 02:42:04 +0800

    first upload part

--- routes/web.php
+++ routes/web.php
@@ -798,7 +798,7 @@ Route::get('/collections', function(){
             ->groupBy('payment_created')
             ->where('unit_property', Auth::user()->property)
             ->whereIn('payment_note',['Rent', 'Electricity', 'Water', 'Surcharge'])
-            ->orderBy('ar_no', 'desc')
+            ->orderBy('ar_number', 'desc')
             ->get();
 
         return view('billing.collections', compact('collections'));
