6d9904b2ca14cb963770193b0fcf533b0deaeda6
unknown <landleydgreat@gmail.com>
2020-07-29 21:32:12 +0800

    asdasd

--- routes/web.php
+++ routes/web.php
@@ -734,12 +734,14 @@ Route::get('/concerns', function(){
 
     if(auth()->user()->status === 'registered' || auth()->user()->user_type === 'admin' || auth()->user()->user_type === 'manager' || auth()->user()->user_type === 'treasury' || auth()->user()->user_type === 'billing'){
         
-            return $concerns = DB::table('tenants')
+             $concerns = DB::table('tenants')
             ->join('units', 'unit_id', 'unit_tenant_id')
             ->join('concerns', 'tenant_id', 'concern_tenant_id')
             
             ->where('unit_property', Auth::user()->property)
             ->get();
+
+            return DB::table('personnels')->get();
        
         return view('admin.concerns', compact('concerns'));
     }else{
