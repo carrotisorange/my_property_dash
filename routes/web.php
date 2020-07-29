8cd8112316d7305e7b32651eef029d6865700ca5
unknown <landleydgreat@gmail.com>
2020-07-30 00:07:32 +0800

    asdasd

--- routes/web.php
+++ routes/web.php
@@ -770,7 +770,7 @@ Route::get('/personnels', function(){
         
               $personnels = DB::table('personnels')
              ->select('*', DB::raw('count(concern_id) as total_concerns'))
-             ->leftJoin('concerns', 'personnel_id', 'concern_personnel_id')
+             ->join('concerns', 'personnel_id', 'concern_personnel_id')
              ->where('concern_status', 'pending')
              ->get();
        
