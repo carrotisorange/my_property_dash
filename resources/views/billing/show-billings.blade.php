b51e1139cd0f7f6f2bcb040bc36884560b913a99
unknown <landleydgreat@gmail.com>
2020-07-30 00:40:26 +0800

    added new columns.

--- resources/views/billing/show-billings.blade.php
+++ resources/views/billing/show-billings.blade.php
@@ -419,6 +419,7 @@
           </div>
           <div class="row">
           <div class="card-body">
+          <p class="text-right"><a class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" ><i class="fas fa-user-edit fa-sm text-white-50"></i> Edit</button></p>
             <p>
                   {{ Auth::user()->note }}
             </p>
