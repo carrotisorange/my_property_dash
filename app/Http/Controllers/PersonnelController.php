60ee17396517e5a2019dc7db37b4147ab2b54039
unknown <landleydgreat@gmail.com>
2020-07-30 00:01:21 +0800

    added the add personnel.

--- app/Http/Controllers/PersonnelController.php
+++ app/Http/Controllers/PersonnelController.php
@@ -37,7 +37,7 @@ class PersonnelController extends Controller
     public function store(Request $request)
     {
         DB::table('personnels')->insert([
-            'personal_name' => $request->personnel_name,
+            'personnel_name' => $request->personnel_name,
             'personnel_contact_no' => $request->personnel_contact_no,
             'personnel_availability' => 'open'
         ]);
