3319164fc33e8c93f72067f4d12ac28c7fe369a6
unknown <landleydgreat@gmail.com>
2020-07-30 01:10:15 +0800

    asdas

--- resources/views/billing/pdf.blade.php
+++ resources/views/billing/pdf.blade.php
@@ -86,11 +86,13 @@
           </table>
           <b>NOTE:</b>
           <div class="row">
-          <div class="card-body">
+         <div class="card">
+         <div class="card-body">
             <p class="text-center">
                   {{ Auth::user()->note }}
             </p>
           </div>
+         </div>
           </div>
           <ul style="list-style-type: none">
             <li><b>Posted by:</b> {{ Auth::user()->name }}</li>
