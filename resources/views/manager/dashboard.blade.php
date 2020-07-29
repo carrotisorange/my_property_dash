3b8999bc40ceee179e79a6e9a377d3d0c3664644
unknown <landleydgreat@gmail.com>
2020-07-29 21:50:22 +0800

    asdasd

--- resources/views/manager/dashboard.blade.php
+++ resources/views/manager/dashboard.blade.php
@@ -411,8 +411,8 @@
                           <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">PENDING CONCERNS</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $pending_concerns+$active_concerns }}</div>
-                            <small>A ({{ active_concerns }})</small>
-                            <small>P ({{ pending_concerns }})</small>
+                            <small>A ({{ $active_concerns }})</small>
+                            <small>P ({{ $pending_concerns }})</small>
                           </div>
                           <div class="col-auto">
                             <i class="fas fa-tools fa-2x text-gray-300"></i>
@@ -476,8 +476,8 @@
                           <div class="col mr-2">
                           <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">PENDING CONCERNS</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $pending_concerns+$active_concerns }}</div>
-                            <small>A ({{ active_concerns }})</small>
-                            <small>P ({{ pending_concerns }})</small>
+                            <small>A ({{ $active_concerns }})</small>
+                            <small>P ({{ $pending_concerns }})</small>
                           </div>
                           <div class="col-auto">
                             <i class="fas fa-tools fa-2x text-gray-300"></i>
