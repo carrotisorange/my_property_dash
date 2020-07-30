6af41095693bf4fdb9c484db87b8b53fa6a23b27
unknown <landleydgreat@gmail.com>
2020-07-30 19:44:32 +0800

    delete the bayani hall.

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Reset Password</title>
+  <title>The Property Manager - Reset Password</title>
 
   <!-- Custom fonts for this template-->
   <link href="{{ asset('dashboard/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
