52c741e140dc3e093c9fc5d8e0c2490f1fec4083
unknown <landleydgreat@gmail.com>
2020-07-30 09:42:20 +0800

    fixed payment footer message.

--- resources/views/billing/pdf.blade.php
+++ resources/views/billing/pdf.blade.php
@@ -84,7 +84,7 @@
              </tr>
             @endif  
           </table>
-          <b>NOTE:</b>
+         
           <div class="row">
           <div class="card-body">
             <p class="text-center">
