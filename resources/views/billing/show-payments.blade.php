f8262a4ec1692ba1e37942a1ad051542b2cb7c44
unknown <landleydgreat@gmail.com>
2020-07-30 11:11:58 +0800

    fixed payment footer message.

--- resources/views/billing/show-payments.blade.php
+++ resources/views/billing/show-payments.blade.php
@@ -378,7 +378,7 @@
                   @endforeach
                       <tr>
                         <th>TOTAL</th>
-                        <th class="text-right">{{ number_format($collection->sum('amt_paid'),2) }}</th>
+                        <th class="text-right">{{ number_format($collection_list->sum('amt_paid'),2) }}</th>
                       </tr>
                 @endforeach
             </table>
