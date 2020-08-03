8feaee8d008cc49c26df9298c9e4d48dae2e22c5
unknown <landleydgreat@gmail.com>
2020-08-03 17:10:55 +0800

    fixed the floor no.

--- resources/views/admin/show-unit.blade.php
+++ resources/views/admin/show-unit.blade.php
@@ -416,7 +416,7 @@
                                   </tr>
                                    @endif
                                    @if($unit->status === 'occupied')
-                                    <tr class="badge badge-primary">
+                                    <tr class="table-secondary">
                                         <th>STATUS</th>
                                         <td>{{ $unit->status }}</td>
                                     </tr>
