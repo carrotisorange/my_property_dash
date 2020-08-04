65c4894598afcf72fd11b72887eb019b21064faf
unknown <landleydgreat@gmail.com>
2020-08-04 19:29:28 +0800

    asdasd

--- resources/views/admin/show-unit.blade.php
+++ resources/views/admin/show-unit.blade.php
@@ -583,7 +583,25 @@
            <div class="card-body">
             <div class="table-responsive">
             <table class="table text-right" width="100%" cellspacing="0" cellpadding="0">
-                  <tr>
+                  
+              </table>
+          </div>
+           </div>
+         </div>
+ 
+            
+        </div>    
+
+        <div class="col-lg-12 mb-4">
+          <!-- DataTales Example -->
+          <div class="card shadow mb-4">
+            <div class="card-header py-3">
+              <h6 class="m-0 font-weight-bold text-primary">BILLING HISTORY</h6>
+            </div>
+           <div class="card-body">
+            <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+            <tr>
                     <th>Bill No</th>
                     <th>Description</th>
                     <th>Period Covered</th>
@@ -604,24 +622,6 @@
                   </tr>
                   @endforeach
             
-              </table>
-          </div>
-           </div>
-         </div>
- 
-            
-        </div>    
-
-        <div class="col-lg-12 mb-4">
-          <!-- DataTales Example -->
-          <div class="card shadow mb-4">
-            <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">BILLING HISTORY</h6>
-            </div>
-           <div class="card-body">
-            <div class="table-responsive">
-            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-               
                   
             </table>
           </div>
