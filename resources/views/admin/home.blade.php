828f7f50c10552e31cbfcd57f1ed2ce795d52581
unknown <landleydgreat@gmail.com>
2020-08-04 11:46:18 +0800

    asdasd

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -333,7 +333,7 @@
                  <div class="d-sm-flex align-items-center justify-content-between mb-4">
                   <h1 class="h3 mb-0 text-gray-800">Home</h1>
                  @if(Auth::user()->user_type === 'manager')
-                 <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" data-toggle="modal" data-target="#addMultipleUnits" data-whatever="@mdo"><i class="fas fa-plus fa-sm text-white-50"></i> Enter room information</a>
+                 <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" data-toggle="modal" data-target="#addMultipleUnits" data-whatever="@mdo"><i class="fas fa-plus fa-sm text-white-50"></i> Add multiple rooms</a>
                  @endif
                 </div>
                 <span class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" ><i class="fas fa-home fa-sm text-white-50"></i> OCCUPIED</span>
