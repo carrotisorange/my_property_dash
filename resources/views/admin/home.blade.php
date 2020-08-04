5df27b138cc16591bed2d99581969c34d7dbb0bd
unknown <landleydgreat@gmail.com>
2020-08-04 11:46:53 +0800

    asdasd

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -573,7 +573,7 @@
   <div class="modal-dialog modal-md" role="document">
   <div class="modal-content">
       <div class="modal-header">
-      <h5 class="modal-title" id="exampleModalLabel" >ADD MULTIPLE ROOMS</h5>
+      <h5 class="modal-title" id="exampleModalLabel" >ENTER ROOM INFORMATION</h5>
 
       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
