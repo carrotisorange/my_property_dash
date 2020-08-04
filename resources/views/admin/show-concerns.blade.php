0ee74e250c4fc30ce764e5a6cd37e52970ee3826
unknown <landleydgreat@gmail.com>
2020-08-04 14:06:48 +0800

    continue working with the concern.

--- resources/views/admin/show-concerns.blade.php
+++ resources/views/admin/show-concerns.blade.php
@@ -287,7 +287,7 @@
 
                 <div class="row">
                 
-                <div class="col-md-6">
+                <div class="col-md-12">
                             <div class="card shadow mb-4">
                                <div class="card-header py-3">
                                  <h6 class="m-0 font-weight-bold text-primary">CONCERN INFORMATION</h6>
