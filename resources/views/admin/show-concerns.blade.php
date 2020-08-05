06e3e48ad6b5678a12f6550ae0f81977d3e0ee77
unknown <landleydgreat@gmail.com>
2020-08-05 13:57:30 +0800

    added the joborders nav bar.

--- resources/views/admin/show-concerns.blade.php
+++ resources/views/admin/show-concerns.blade.php
@@ -88,11 +88,18 @@
      @endif
   
         <!-- Nav Item - Tables -->
-      <li class="nav-item active">
-          <a class="nav-link" href="/concerns">
-            <i class="fas fa-tools fa-table"></i>
-            <span>Concerns</span></a>
-        </li>
+           <!-- Nav Item - Tables -->
+      <li class="nav-item">
+      <a class="nav-link" href="/concerns">
+        <i class="fas fa-tools fa-table"></i>
+        <span>Concerns</span></a>
+  </li>
+
+  <li class="nav-item">
+      <a class="nav-link" href="/job-orders">
+        <i class="fas fa-tools fa-table"></i>
+        <span>Job Orders</span></a>
+  </li>
 
               <!-- Nav Item - Tables -->
                <li class="nav-item">
