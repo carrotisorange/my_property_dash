161490b3ab9682bb9bc53b30536008162a3dd06b
unknown <landleydgreat@gmail.com>
2020-08-05 14:23:40 +0800

    added edit option in show-concern.

--- resources/views/admin/show-concerns.blade.php
+++ resources/views/admin/show-concerns.blade.php
@@ -251,18 +251,20 @@
                                                <!-- Card Header - Dropdown -->
                               <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                                 <h6 class="m-0 font-weight-bold text-primary">CONCERN INFORMATION</h6>
+                                <!-- start -->
                                 <div class="dropdown no-arrow">
                                   <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                   <i class="fas fa-edit fa-sm fa-fw text-gray-400"></i>
                                   </a>
-                                  <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                                  <!-- <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
                                     <div class="dropdown-header">Dropdown Header:</div>
                                     <a class="dropdown-item" href="#">Action</a>
                                     <a class="dropdown-item" href="#">Another action</a>
                                     <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="#">Something else here</a>
+                                    <a class="dropdown-item" href="#">Something else here</a> -->
                                   </div>
                                 </div>
+                                <!-- end -->
                               </div>
                                <div class="card-body">
                             <div class="table-responsive">
