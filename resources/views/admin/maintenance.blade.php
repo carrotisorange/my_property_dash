6fd557474a4c1d890c5d6e260afa7668000b4723
unknown <landleydgreat@gmail.com>
2020-08-05 11:08:28 +0800

    added dropdown in personnels nav bar.

--- resources/views/admin/maintenance.blade.php
+++ resources/views/admin/maintenance.blade.php
@@ -340,15 +340,76 @@
         </nav>
         <!-- End of Topbar -->
         <div class="container-fluid">
-         
-        <div class="d-sm-flex align-items-center justify-content-between mb-4">
-          <h1 class="h3 mb-0 text-gray-800">Maintenance</h1>
-        </div>
-        
-    
-        
-        </div>
+        @foreach (['danger', 'warning', 'success', 'info'] as $key)
+          @if(Session::has($key))
+         <p class="alert alert-{{ $key }}"> <i class="fas fa-check-circle"></i> {{ Session::get($key) }}</p>
+          @endif
+          @endforeach
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Housekeeping</h1>
+            <a class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls=""> <i class="fas fa-user-plus  fa-sm text-white-50"></i> Add Maintenance</a> 
+            
+          </div>
 
+          <div class="row">
+            <div class="col">
+              <div class="collapse multi-collapse" id="multiCollapseExample1">
+                <div class="card card-body">
+                    <form id="addPersonnelForm" action="/personnels" method="POST">
+                        {{ csrf_field() }}
+                    </form>
+                    <div class="row">
+                        <div class="col">
+                            <label for="recipient-name" class="col-form-label"><b>Name</b></label>
+                            <input form="addPersonnelForm" type="text" class="form-control" name="personnel_name" required>
+                            <input form="addPersonnelForm" type="hidden" class="form-control" name="personnel_type" value="maintenance" required>
+                        </div>
+                        <div class="col">
+                            <label for="recipient-name" class="col-form-label"><b>Contact No</b></label>
+                            <input form="addPersonnelForm" type="text" class="form-control" name="personnel_contact_no" required>
+                        </div>
+                       
+                    </div>
+                    <br>
+                      <p class="text-right">
+                        <button class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" type="submit" form="addPersonnelForm" onclick="return confirm('Are you sure you want perform this action?');" ><i class="fas fa-check fa-sm text-white-50"></i> Add Housekeeping</button>
+                      </p>
+
+                </div>
+              </div>
+            </div>
+        </div>
+        <br>
+              <div class="table-responsive">
+               
+                  <table class="table table-bordered" width="100%" cellspacing="0">
+                    <thead>
+                      <tr>
+                          <th>ID</th>
+                          <th>PERSONNEL</th>
+                          <th>CONTACT NO</th>
+                          <th>AVAILABILITY</th>
+                          <th>PERSONNEL TYPE</th>
+                     </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($maintenance as $item)
+                    <tr>
+                      <td>{{ $item->personnel_id }}</td>
+                        <td>{{ $item->personnel_name }}</td>
+                        <td>{{ $item->personnel_contact_no }}</td>
+                        <td>{{ $item->personnel_availability }}</td>
+                        <td>{{ $item->personnel_type }}</td>
+                    </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
+                  
+                </div>
+          
+          </div>
+        </div>
+                    </div>
       </div>
       <!-- End of Main Content -->
 
