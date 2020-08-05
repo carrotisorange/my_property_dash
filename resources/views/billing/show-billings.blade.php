63d2f0c6b0e983e7b44d7aaff8f7a2e56fb78ec6
unknown <landleydgreat@gmail.com>
2020-08-05 16:19:35 +0800

    added concerns in dashboard.

--- resources/views/billing/show-billings.blade.php
+++ resources/views/billing/show-billings.blade.php
@@ -594,33 +594,31 @@
             </div>
 
 
-            <div class="modal fade" id="addBill" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog modal-lg" role="document">
-              <div class="modal-content">
-                  <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">Enter Bill Information</h5>
-                  
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                  </button>
-                  </div>
-                  <div class="modal-body">
-                      <form id="addBillForm" action="/billings" method="POST">
-                      {{ csrf_field() }}
-                      </form>
-                    
-                     
-                     
-                    </div>
-                  </div>
-                  <div class="modal-footer">
-                      <button type="button" class="d-none d-sm-inline-block btn btn-sm btn-secondary shadow-sm" data-dismiss="modal"><i class="fas fa-times fa-sm text-white-50"></i> Cancel</button>
-                      <button form="addBillForm" id ="addPaymentButton" type="submit" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" onclick="return confirm('Are you sure you want perform this action?');" ><i class="fas fa-check fa-sm text-white-50f"></i> Add Payment</button>
-                  </div>
-           
-              </div>
-              </div>
         </div>
+        <div class="modal fade" id="addBill" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-md" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Enter Bill Information</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                <div class="modal-body">
+                <form id="editConcernDetailsForm" action="/concerns/{{ $concern->concern_id }}" method="POST">
+                  @method('put')
+                  {{ csrf_field() }}
+                </form>
+                  
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="d-none d-sm-inline-block btn btn-sm btn-secondary shadow-sm" data-dismiss="modal"><i class="fas fa-times fa-sm text-white-50"></i> Cancel</button>
+                    <button form="editConcernDetailsForm" type="submit" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" onclick="return confirm('Are you sure you want to perform this action?');" ><i class="fas fa-check fa-sm text-white-50"></i> Update</button>
+                </div>
+            </div>
+            </div>
+        
         </div>
 
       </div>
