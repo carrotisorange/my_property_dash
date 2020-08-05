8ac70bb00f8d6cf7ee0b03edd9b276617805bab1
unknown <landleydgreat@gmail.com>
2020-08-05 10:29:17 +0800

    added dropdown in personnels nav bar.

--- resources/views/manager/dashboard.blade.php
+++ resources/views/manager/dashboard.blade.php
@@ -100,33 +100,17 @@
        <!-- Nav Item - Tables -->
 
        <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-          <i class="fas fa-fw fa-cog"></i>
-          <span>Components</span>
+        <a class="nav-link collapsed" href="/personnels" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+        <i class="fas fa-user-cog"></i>
+          <span>Personnels</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="buttons.html">Buttons</a>
-            <a class="collapse-item" href="cards.html">Cards</a>
+            <a class="collapse-item" href="/housekeeping">Housekeeping</a>
+            <a class="collapse-item" href="/maintenance">Maintenance</a>
           </div>
         </div>
       </li>
-
-  <!-- <li class="nav-item">
-      <a class="nav-link" href="/personnels">
-      <i class="fas fa-user-cog"></i>
-        <span>Personnels</span></a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="buttons.html">Buttons</a>
-            <a class="collapse-item" href="cards.html">Cards</a>
-          </div>
-        </div>
-  </li> -->
-
-     
     @endif
 
      @if(Auth::user()->user_type === 'billing' || Auth::user()->user_type === 'manager')
