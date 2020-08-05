ceac1f3ac153640e55338e1bf19ed80335a7723d
unknown <landleydgreat@gmail.com>
2020-08-05 14:10:27 +0800

    added feedback column in concerns table.

--- resources/views/admin/show-concerns.blade.php
+++ resources/views/admin/show-concerns.blade.php
@@ -326,10 +326,23 @@
                 <div class="col-md-12">
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">ACTION THAT HAS BEEN TAKEN TO THE CONCERN</h6>            
+                            <h6 class="m-0 font-weight-bold text-primary">ACTION THAT HAS BEEN TAKEN TO ADDRESS THE CONCERN</h6>            
                         </div>
                         <div class="card-body">
                             <p>{{ $concern->action_taken }}</p>
+                        </div>
+                    </div>    
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">FEEDBACK BY THE TENANT TO THE CONCERN</h6>            
+                        </div>
+                        <div class="card-body">
+                            <p>{{ $concern->feedback }}</p>
                         </div>
                     </div>    
                 </div>
