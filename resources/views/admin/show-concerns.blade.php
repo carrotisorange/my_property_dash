f9445b14dfc0fa9f32a932a764ab2be49899c5b7
unknown <landleydgreat@gmail.com>
2020-08-05 15:34:58 +0800

    added edit option in show-concern.

--- resources/views/admin/show-concerns.blade.php
+++ resources/views/admin/show-concerns.blade.php
@@ -89,7 +89,7 @@
   
         <!-- Nav Item - Tables -->
            <!-- Nav Item - Tables -->
-      <li class="nav-item">
+      <li class="nav-item active">
       <a class="nav-link" href="/concerns">
         <i class="fas fa-tools fa-table"></i>
         <span>Concerns</span></a>
