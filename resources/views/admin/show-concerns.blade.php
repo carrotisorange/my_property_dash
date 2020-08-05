79ad4263e774538890968aa457445dd2d73dbcdf
unknown <landleydgreat@gmail.com>
2020-08-05 14:41:46 +0800

    added edit option in show-concern.

--- resources/views/admin/show-concerns.blade.php
+++ resources/views/admin/show-concerns.blade.php
@@ -369,13 +369,21 @@
             <div class="modal-content">
                 <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Edit Concern Information</h5>
-        
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
                 </div>
                 <div class="modal-body">
+                <form id="editConcernDetailsForm" action="/concerns/{{ $concern->concern_id }" method="POST">
+                  @method('put')
+                  {{ csrf_field() }}
+                </form>
                   
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="d-none d-sm-inline-block btn btn-sm btn-secondary shadow-sm" data-dismiss="modal"><i class="fas fa-times fa-sm text-white-50"></i> Cancel</button>
+                    <button form="editConcernDetailsForm" type="submit" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" onclick="return confirm('Are you sure you want to perform this action?');" ><i class="fas fa-check fa-sm text-white-50"></i> Update</button>
                 </div>
             </div>
             </div>
