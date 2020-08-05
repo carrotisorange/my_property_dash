e189095a8ab16a7a696e72da2912b2d76701bc86
unknown <landleydgreat@gmail.com>
2020-08-05 10:25:29 +0800

    added the account payables nav bar.

--- resources/views/manager/dashboard.blade.php
+++ resources/views/manager/dashboard.blade.php
@@ -102,6 +102,13 @@
       <a class="nav-link" href="/personnels">
       <i class="fas fa-user-cog"></i>
         <span>Personnels</span></a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Custom Components:</h6>
+            <a class="collapse-item" href="buttons.html">Buttons</a>
+            <a class="collapse-item" href="cards.html">Cards</a>
+          </div>
+        </div>
   </li>
 
      
