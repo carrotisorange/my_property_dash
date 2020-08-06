2c6f4b5a801dc1e8e56a0fa6cd4fca4a3b987a72
unknown <landleydgreat@gmail.com>
2020-08-06 14:19:17 +0800

    fixed concern form.

--- resources/views/admin/show-unit.blade.php
+++ resources/views/admin/show-unit.blade.php
@@ -648,7 +648,7 @@
                    <tr>
                        <th>ID</th>
                        <th>DATE REPORTED</th>
-                      
+                      <th>TENANT</th>
                        <th>ROOM</th>
                        <th>TYPE</th>
                        <th>DESCRIPTION</th>
@@ -662,7 +662,9 @@
                    <tr>
                    <td>{{ $item->concern_id }}</td>
                      <td>{{ Carbon\Carbon::parse($item->date_reported)->format('M d Y') }}</td>
-                       
+                     <td>
+                            <a href="{{ route('show-tenant',['unit_id'=> $item->unit_id, 'tenant_id'=>$item->tenant_id]) }}">{{ $item->first_name.' '.$item->last_name }}</a>
+                        </td>
                        <td>{{ $item->building.' '.$item->unit_no }}</td>
                        <td>
                          
