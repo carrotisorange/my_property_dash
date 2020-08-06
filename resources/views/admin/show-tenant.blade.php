472ab7179aa59a6d949a05690ae3b0a9be3d166f
unknown <landleydgreat@gmail.com>
2020-08-06 14:40:16 +0800

    fixed concern form.

--- resources/views/admin/show-tenant.blade.php
+++ resources/views/admin/show-tenant.blade.php
@@ -551,11 +551,11 @@
                 </div>
               </div>
 
-              <div class="row">
+              <div class="row" id="concerns-history">
                 <div class="col-md-12">
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">CONCERNS</h6>            
+                            <h6 class="m-0 font-weight-bold text-primary">CONCERNS HISTORY</h6>            
                         </div>
                         <div class="card-body">
                         <div class="table-responsive">
