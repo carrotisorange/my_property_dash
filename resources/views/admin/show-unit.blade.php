b3d262ac0f2255d13bef705df5066cad9c7edbb5
unknown <landleydgreat@gmail.com>
2020-08-07 14:44:52 +0800

    asdas

--- resources/views/admin/show-unit.blade.php
+++ resources/views/admin/show-unit.blade.php
@@ -798,7 +798,7 @@
                         <div class="modal-dialog" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">ADD UNIT OWNER</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">Enter the owner information</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
