fba8d046c9896c1a3cff67a1555295dbf30ecb6f
unknown <landleydgreat@gmail.com>
2020-08-10 16:24:07 +0800

    asdasd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,124 +1,75 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-    <title>@yield('title')</title>
+  <title>@yield('title')</title>
 
-      <!-- Favicons -->
+  <!-- Custom fonts for this template-->
+  <link href="{{ asset('dashboard/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-    <link href="{{ asset('index/assets/img/favicon.png') }}" rel="icon">
-    <link href="{{ asset('index/assets/img/favicon.png') }}" rel="apple-touch-icon">
+  <!-- Custom styles for this template-->
+  <link href="{{ asset('dashboard/css/sb-admin-2.min.css') }}" rel="stylesheet">
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-    <!-- Scripts -->
-    <script src="//code.jquery.com/jquery-2.1.3.min.js"></script>
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    {{-- font awesome --}}
-    <script src="https://kit.fontawesome.com/77bb6f2aef.js" crossorigin="anonymous"></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-    {{-- ChartStyle --}}
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                @guest
-                <a class="navbar-brand" href="{{ url('/login') }}">
-                    <b>The Property Manager</b>
-                    <h6>The Property Management System of GoDie</h6>
-                </a> 
-                @else
-                <a class="navbar-brand" href="{{ url('/login') }}">
-                    {{Auth::user()->property }}
-                </a>
-                
-                @endguest
-               
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
+<body id="page-top">
+    <div class="container">
+         @yield('content')
+         <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
                 </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                         {{-- <li class="nav-item">
-                                <h5><a class="nav-link  btn" href="/register">Register your property <i class="fas fa-users-circle"></i></a></h5>
-                            </li>
-                            {{-- <li class="nav-item">
-                                <h5><a class="nav-link  btn" href="/register">Register your property <i class="fas fa-users-circle"></i></a></h5>
-                            </li>
-                            <li class="nav-item">
-                                <h5><a class="nav-link  btn" href="/properties">Make a reservation <i class="far fa-calendar-check"></i></a></h5>
-                            </li> --}}
-                            {{-- <li class="nav-item">
-                                <h5><a class="nav-link btn " href="/faq">FAQ <i class="fas fa-question-circle"></i></a></h5>
-                            </li> --}}
-                           
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">Register</a>
-                                </li>
-                            @endif 
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{  explode(' ',trim(Auth::user()->name))[0] }} <span class="caret"></span>
-                                </a>
-                                
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="users/{{ Auth::user()->id }}">Profile</a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-                                   
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                <a class="btn btn-danger" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+                    Logout
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
                 </div>
             </div>
-        </nav>
-
-        <main class="py-4">
-            @include('notifications')
-            @yield('content')
-        </main>
+            </div>
+        </div>
+        <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Godie Enterprises 2020</span>
+          </div>
+        </div>
+      </footer>
     </div>
-</body>
-<script>
-    $(document).ready(function() {
-        $(document).on('submit', 'form', function() {
-            $('button').attr('disabled', 'disabled');
-        });
-    });
-</script>
     
+</body>
+@push('scripts')
+   <!-- Bootstrap core JavaScript-->
+  <script src="{{ asset('/dashboard/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('/dashboard/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="{{ asset('/dashboard/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="{{ asset('/dashboard/js/sb-admin-2.min.js') }}"></script>
+
+  <!-- Page level plugins -->
+  <script src="{{ asset('/dashboard/vendor/chart.js/Chart.min.js') }}"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="{{ asset('/dashboard/js/demo/chart-area-demo.js') }}"></script>
+  <script src="{{ asset('/dashboard/js/demo/chart-pie-demo.js') }}"></script>
+@endpush   
 </html>
