69fbddd6e04b8dbaa9990005cc8406d87fb3b49b
unknown <landleydgreat@gmail.com>
2020-08-10 14:07:20 +0800

    asdasd

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -27,7 +27,7 @@ class ConfirmPasswordController extends Controller
      * @var string
      */
     // protected $redirectTo = RouteServiceProvider::HOME;
-    protected $redirectTo = '/';
+    protected $redirectTo = '/board';
 
     /**
      * Create a new controller instance.
