98d973bf8f5776a1abaab6e0ae3bd0ae4da9c35c
unknown <landleydgreat@gmail.com>
2020-08-10 13:27:54 +0800

    asdasd

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -207,6 +207,12 @@
         </nav>
         <!-- End of Topbar -->
         <div class="container-fluid">
+        <div class="text-center">
+            <div class="error mx-auto" data-text="404">404</div>
+            <p class="lead text-gray-800 mb-5">Page Not Found</p>
+            <p class="text-gray-500 mb-0">Please wait. Your account is being reviewed...</p>
+           
+          </div>
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     <div class="card">
