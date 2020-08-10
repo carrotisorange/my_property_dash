64d774f84a03fa806925eb50903fbb38f6768e70
unknown <landleydgreat@gmail.com>
2020-08-10 10:01:49 +0800

    asdasd

--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -68,7 +68,7 @@
           <li><a href="#services">Services</a></li>    
           <li><a href="#pricing">Pricing</a></li>
           <li><a href="#contact">Contact</a></li>
-          <li><a class="btn" target="_blank" href="/login">Login</a></li>
+          <li><a class="btn-get-started" target="_blank" href="/login">Login</a></li>
         </ul>
       </nav><!-- .nav-menu -->
 
