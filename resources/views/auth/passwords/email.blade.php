f17d42eb89f756d44a646259cd82771fe559d755
unknown <landleydgreat@gmail.com>
2020-08-10 10:55:55 +0800

    asdasd

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -82,8 +82,10 @@
                   </div> -->
                   <div class="text-center">
                     <a class="small" href="/register">Create an Account!</a>
-                    <a class="small" href="/login">Already have an account? Login!</a>
                   </div>
+                  <div class="text-center">
+                <a class="small" href="/login">Already have an account? Login!</a>
+              </div>
                 </div>
               </div>
             </div>
