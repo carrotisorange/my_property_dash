94c51c4f2d447686cfd699202c356c6c0296b3a1
unknown <landleydgreat@gmail.com>
2020-08-10 13:30:02 +0800

    asdasd

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -210,31 +210,21 @@
         <div class="text-center">
             <div class="error mx-auto" data-text="404">404</div>
             <p class="lead text-gray-800 mb-5">Page Not Found</p>
-            <p class="text-gray-500 mb-0">Please wait. Your account is being reviewed...</p>
+            <p class="text-gray-500 mb-0">
+            @if (session('resent'))
+                <div class="alert alert-success" role="alert">
+                  {{ __('A fresh verification link has been sent to your email address.') }}
+                </div>
+            @endif
+                  {{ __('Before proceeding, please check your email for a verification link.') }}
+                  {{ __('If you did not receive the email') }},
+                  <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                    @csrf
+                    <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                    </form>
+            </p>
            
           </div>
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-                        <div class="card-body">
-                            @if (session('resent'))
-                                <div class="alert alert-success" role="alert">
-                                    {{ __('A fresh verification link has been sent to your email address.') }}
-                                </div>
-                            @endif
-
-                            {{ __('Before proceeding, please check your email for a verification link.') }}
-                            {{ __('If you did not receive the email') }},
-                            <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                                @csrf
-                                <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
 
       </div>
