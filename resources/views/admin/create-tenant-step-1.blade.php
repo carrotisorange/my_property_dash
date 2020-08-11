5ba85f9978dbbd44d7eb4f4732c703a67f7d4798
unknown <landleydgreat@gmail.com>
2020-08-11 20:07:56 +0800

    asdasd

--- resources/views/admin/create-tenant-step-1.blade.php
+++ resources/views/admin/create-tenant-step-1.blade.php
@@ -568,27 +568,21 @@ select.list-dt:focus {
            <!-- MultiStep Form -->
            
                 <div class="row justify-content-center mt-0">
-                    <div class="col-11 col-sm-9 col-md-7 col-lg-6 text-center p-0 mt-3 mb-2">
-                        <div class="card px-0 pt-4 pb-0 mt-3 mb-3">
-
+                    
+                        
                             <div class="row">
                                 <div class="col-md-12 mx-0">
-                                   
                                         <!-- progressbar -->
                                         <ul id="progressbar">
                                             <li class="active" id="account"><strong>Account</strong></li>
                                             <li id="personal"><strong>Personal</strong></li>
                                             <li id="payment"><strong>Payment</strong></li>
                                             <li id="confirm"><strong>Finish</strong></li>
-                                        </ul> <!-- fieldsets -->
-                                       
-                                      
-                                 
-                                  
+                                        </ul> <!-- fieldsets -->   
                                 </div>
                             </div>
-                        </div>
-                    </div>
+                      
+               
                 </div>
           
 
