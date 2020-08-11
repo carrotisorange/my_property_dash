c6cc65d85c07a0c3c68b31426408b8f49d0fb8c0
unknown <landleydgreat@gmail.com>
2020-08-11 18:03:30 +0800

    asdasd

--- resources/views/billing/add-billings.blade.php
+++ resources/views/billing/add-billings.blade.php
@@ -378,7 +378,7 @@
                 <td>{{ $item->building.' '.$item->unit_no }}</td>
                 <td>
                   @if($item->tenants_note !== 'new' )
-                    <input form="add_billings" type="text" class='col-md-8' name="details{{ $details_ctr++  }}" value="{{ Carbon\Carbon::parse($item->movein_date)->startOfMonth()->format('M d') }}-{{ Carbon\Carbon::now()->endOfMonth()->format('d Y') }} " >
+                    <input form="add_billings" type="text" class='col-md-8' name="details{{ $details_ctr++  }}" value="{{ Carbon\Carbon::now()->addDay()->format('M d') }}-{{ Carbon\Carbon::now()->endOfMonth()->format('d Y') }} " >
                   @else
                     <input form="add_billings" type="text" class='col-md-8' name="details{{ $details_ctr++  }}" value="{{ Carbon\Carbon::now()->startOfMonth()->format('M d') }}-{{ Carbon\Carbon::now()->endOfMonth()->format('d Y') }} " >
                   @endif
