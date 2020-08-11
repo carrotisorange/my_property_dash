124908393af48b8e23a85cf0e98e4325365a7830
unknown <landleydgreat@gmail.com>
2020-08-11 19:51:50 +0800

    asdasd

--- resources/views/admin/create-tenant-step-1.blade.php
+++ resources/views/admin/create-tenant-step-1.blade.php
@@ -336,43 +336,46 @@
         <div class="container-fluid">
             <!-- Page Heading -->
     
-          <!-- Horizontal Steppers -->
-        <div class="row">
-          <div class="col-md-12">
-
-            <!-- Stepers Wrapper -->
-            <ul class="stepper stepper-horizontal">
-
-              <!-- First Step -->
-              <li class="completed">
-                <a href="#!">
-                  <span class="circle">1</span>
-                  <span class="label">First step</span>
-                </a>
-              </li>
-
-              <!-- Second Step -->
-              <li class="active">
-                <a href="#!">
-                  <span class="circle">2</span>
-                  <span class="label">Second step</span>
-                </a>
-              </li>
-
-              <!-- Third Step -->
-              <li class="warning">
-                <a href="#!">
-                  <span class="circle"><i class="fas fa-exclamation"></i></span>
-                  <span class="label">Third step</span>
-                </a>
-              </li>
-
-            </ul>
-            <!-- /.Stepers Wrapper -->
-
-          </div>
+            <ul class="stepper horizontal" id="horizontal-stepper">
+  <li class="step active">
+    <div class="step-title waves-effect waves-dark">Step 1</div>
+    <div class="step-new-content">
+      <div class="row">
+        <div class="md-form col-12 ml-auto">
+          <input id="email-horizontal" type="email" class="validate form-control" required>
+          <label for="email-horizontal">Email</label>
         </div>
-        <!-- /.Horizontal Steppers -->
+      </div>
+      <div class="step-actions">
+        <button class="waves-effect waves-dark btn btn-sm btn-primary next-step" data-feedback="someFunction21">CONTINUE</button>
+      </div>
+    </div>
+  </li>
+  <li class="step">
+    <div class="step-title waves-effect waves-dark">Step 2</div>
+    <div class="step-new-content">
+      <div class="row">
+        <div class="md-form col-12 ml-auto">
+          <input id="password-horizontal" type="password" class="validate form-control" required>
+          <label for="password-horizontal">Your password</label>
+        </div>
+      </div>
+      <div class="step-actions">
+        <button class="waves-effect waves-dark btn btn-sm btn-primary next-step" data-feedback="someFunction21">CONTINUE</button>
+        <button class="waves-effect waves-dark btn btn-sm btn-secondary previous-step">BACK</button>
+      </div>
+    </div>
+  </li>
+  <li class="step">
+    <div class="step-title waves-effect waves-dark">Step 3</div>
+    <div class="step-new-content">
+      Finish!
+      <div class="step-actions">
+        <button class="waves-effect waves-dark btn-sm btn btn-primary m-0 mt-4" type="button">SUBMIT</button>
+      </div>
+    </div>
+  </li>
+</ul>
         
           @foreach (['danger', 'warning', 'success', 'info'] as $key)
           @if(Session::has($key))
