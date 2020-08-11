59d4bb42c1a620c929f010adcbd8e8b5730c1940
unknown <landleydgreat@gmail.com>
2020-08-11 18:06:06 +0800

    asdasd

--- resources/views/billing/add-billings.blade.php
+++ resources/views/billing/add-billings.blade.php
@@ -386,14 +386,10 @@
                 <td>
                 <?php 
                       $prorated_rent =  number_format(Carbon\Carbon::now()->addDay()->DiffInDays(Carbon\Carbon::now()->endOfMonth()));
-                      $prorated_monthly_rent =  ($item->tenant_monthly_rent/30) * $prorated_rent;
+                      $monthly_rent =  ($item->tenant_monthly_rent/30) * $prorated_rent;
                 
                 ?>
-                  @if($item->tenants_note === 'new' )
-                    <input form="add_billings" class="col-md-6" type="number" name="amt{{ $amt_ctr++ }}" step="0.01"  value="{{ $item->tenant_monthly_rent }}" oninput="this.value = Math.abs(this.value)">
-                  @else
-                    <input form="add_billings" class="col-md-6" type="number" name="amt{{ $amt_ctr++ }}" step="0.01"  value="{{ $prorated_monthly_rent }}" oninput="this.value = Math.abs(this.value)">
-                  @endif
+                    <input form="add_billings" class="col-md-6" type="number" name="amt{{ $amt_ctr++ }}" step="0.01"  value="{{ $monthly_rent }}" oninput="this.value = Math.abs(this.value)">
                 </td>
            </tr>
            @endforeach
