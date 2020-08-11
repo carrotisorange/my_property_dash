34dfcc227ac2adfabd1e206d5a79984c1fbb1752
unknown <landleydgreat@gmail.com>
2020-08-11 10:56:45 +0800

    asdasd

--- resources/views/admin/show-concerns.blade.php
+++ resources/views/admin/show-concerns.blade.php
@@ -582,29 +582,29 @@
         </div>
 
         <div class="modal fade" id="createJobOrderModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-md" role="document">
+            <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Enter the feedback of the tenant to the concern</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Enter Job Order Information</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
                 </div>
                 <div class="modal-body">
-                <form id="editConcernDetailsForm" action="/concerns/{{ $concern->concern_id }}" method="POST">
+                <form id="editConcernDetailsForm" action="/joborders/{{ $concern->concern_id }}" method="POST">
                   @method('put')
                   {{ csrf_field() }}
                 </form>
                   <div class="row">
                       <div class="col">
                           <small></small>
-                          <textarea form="editConcernDetailsForm" class="form-control" name="feedback" cols="30" rows="10">{{ $concern->feedback }}</textarea>
+                         
                       </div>
                   </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="d-none d-sm-inline-block btn btn-sm btn-secondary shadow-sm" data-dismiss="modal"><i class="fas fa-times fa-sm text-white-50"></i> Cancel</button>
-                    <button form="editConcernDetailsForm" type="submit" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" onclick="return confirm('Are you sure you want to perform this action?');" ><i class="fas fa-check fa-sm text-white-50"></i> Update</button>
+                    <button form="editConcernDetailsForm" type="submit" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" onclick="return confirm('Are you sure you want to perform this action?');" ><i class="fas fa-check fa-sm text-white-50"></i> Create Job Order</button>
                 </div>
             </div>
             </div>
