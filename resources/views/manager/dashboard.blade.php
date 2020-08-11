04c530fe5b7817cbb20a4c56618531f3ec1f0917
unknown <landleydgreat@gmail.com>
2020-08-11 15:50:32 +0800

    asdasd

--- resources/views/manager/dashboard.blade.php
+++ resources/views/manager/dashboard.blade.php
@@ -255,7 +255,7 @@
                   </div>
                   <div>
                     <div class="small text-gray-500">{{Carbon\Carbon::parse($item->created_at)->format('M d Y')}}</div>
-                    <span class="font-weight-bold">{{ $item->building.' '.$item->unit_no }} request request to moveout has been approved.</span>
+                    <span class="font-weight-bold">{{ $item->building.' '.$item->unit_no }} request to moveout has been approved.</span>
                   </div>
                 </a> 
                 @endforeach 
