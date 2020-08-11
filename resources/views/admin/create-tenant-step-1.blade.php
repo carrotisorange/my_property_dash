4e0386de306d19c1ce749fb49290f460c2947919
unknown <landleydgreat@gmail.com>
2020-08-11 19:50:01 +0800

    asdasd

--- resources/views/admin/create-tenant-step-1.blade.php
+++ resources/views/admin/create-tenant-step-1.blade.php
@@ -336,9 +336,43 @@
         <div class="container-fluid">
             <!-- Page Heading -->
     
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item active" aria-current="page">Data</li>
-            </ol>
+          <!-- Horizontal Steppers -->
+        <div class="row">
+          <div class="col-md-12">
+
+            <!-- Stepers Wrapper -->
+            <ul class="stepper stepper-horizontal">
+
+              <!-- First Step -->
+              <li class="completed">
+                <a href="#!">
+                  <span class="circle">1</span>
+                  <span class="label">First step</span>
+                </a>
+              </li>
+
+              <!-- Second Step -->
+              <li class="active">
+                <a href="#!">
+                  <span class="circle">2</span>
+                  <span class="label">Second step</span>
+                </a>
+              </li>
+
+              <!-- Third Step -->
+              <li class="warning">
+                <a href="#!">
+                  <span class="circle"><i class="fas fa-exclamation"></i></span>
+                  <span class="label">Third step</span>
+                </a>
+              </li>
+
+            </ul>
+            <!-- /.Stepers Wrapper -->
+
+          </div>
+        </div>
+        <!-- /.Horizontal Steppers -->
         
           @foreach (['danger', 'warning', 'success', 'info'] as $key)
           @if(Session::has($key))
