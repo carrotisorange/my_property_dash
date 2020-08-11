f3d80c3f6fcbd8ccc33f2350e1ffe367712288e3
unknown <landleydgreat@gmail.com>
2020-08-11 10:41:21 +0800

    asdasd

--- resources/views/admin/show-concerns.blade.php
+++ resources/views/admin/show-concerns.blade.php
@@ -332,17 +332,19 @@
                               <h6 class="m-0 font-weight-bold text-primary">DETAILS OF THE CONCERN</h6>
                                 <!-- start -->
                                 <div class="dropdown no-arrow">
-                                  <a href="#" class="dropdown-toggle" data-toggle="modal" data-target="#editConcernFullDetails" >
-                                  <i class="fas fa-edit fa-sm fa-fw text-gray-400"></i>
+                                  <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
                                   </a>
-                                  <!-- <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                                  <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
                                     <div class="dropdown-header">Dropdown Header:</div>
-                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" data-toggle="modal" data-target="#editConcernFullDetails">Edit details</a>
+                                    
                                     <a class="dropdown-item" href="#">Another action</a>
                                     <div class="dropdown-divider"></div>
                                     <a class="dropdown-item" href="#">Something else here</a>
-                                  </div> -->
+                                  </div> 
                                 </div>
+                              
                                 <!-- end -->
                               </div>
                     
