a0d85eb37ad5cd4d459bc2787a016f1759c549e8
unknown <landleydgreat@gmail.com>
2020-08-13 13:19:53 +0800

    asdas

--- resources/views/all-notifications.blade.php
+++ resources/views/all-notifications.blade.php
@@ -383,7 +383,7 @@
                   </div>
                   <div>
                     <div class="small text-gray-500">{{Carbon\Carbon::parse($item->updated_at)->format('M d Y')}}</div>
-                    <span class="font-weight-bold">{{ $item->building.' '.$item->unit_no }} has sent a request to moveout.</span>
+                    <span class="font-weight-bold">{{ $item->building.' '.$item->unit_no }} request to moveout has been approved.</span>
                   </div>
                 </a> 
                 <hr>
