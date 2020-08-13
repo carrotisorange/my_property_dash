990e753577b01c6affbde52e75470d3514502e41
unknown <landleydgreat@gmail.com>
2020-08-14 01:49:44 +0800

    asdas

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS'),
+        'address' => env('MAIL_FROM_ADDRESS', 'lmbernardo@slu.edu.ph'),
         'name' => env('MAIL_FROM_NAME'),
     ],
 
