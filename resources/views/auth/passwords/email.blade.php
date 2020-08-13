b0f3f65b4e40dcfdd11b82e75870c62c3e6979c0
unknown <landleydgreat@gmail.com>
2020-08-14 02:05:32 +0800

    asdas

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -51,7 +51,7 @@
                         {{  session('status') }}
                     </div>
                   @endif
-                   <form class="user" id="resetPasswordForm" class="user" method="POST" action="{{ route('password.email') }}">
+                   <form class="user" id="resetPasswordForm" method="POST" action="{{ route('password.email') }}">
                     @csrf
                 
                     <div class="form-group">
