7beb0e81bff6f3041d95f5ec2acca92ef899e9fc
unknown <landleydgreat@gmail.com>
2020-08-13 22:35:52 +0800

    asdas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -77,7 +77,7 @@
                   <hr>
                   <div class="text-center">
                     @if (Route::has('password.request'))
-                        <a class="small" btn-link" href="{{ route('password.request') }}">
+                        <a class="small btn-link" href="{{ route('password.request') }}">
                             {{ __('Forgot Your Password?') }}
                         </a>
                     @endif
