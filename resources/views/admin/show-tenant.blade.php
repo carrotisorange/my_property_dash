f57116b616e7b800ed3701b1d77e8e91667c58bd
unknown <landleydgreat@gmail.com>
2020-08-13 09:52:34 +0800

    asdas

--- resources/views/admin/show-tenant.blade.php
+++ resources/views/admin/show-tenant.blade.php
@@ -398,11 +398,11 @@
                           @if($tenant->created_at !== null)
                           <tr>
                               <td>Requested moveout at</td>
-                              <td>{{ Carbon\Carbon::parse($tenant->created_at)->format('Y-m-d') }}</td>
+                              <td>{{ Carbon\Carbon::parse($tenant->created_at)->format('M d Y') }}</td>
                           </tr>
                           <tr>
                               <td>Approved moveout at</td>
-                              <td>{{ Carbon\Carbon::parse($tenant->updated_at)->format('Y-m-d') }}</td>
+                              <td>{{ Carbon\Carbon::parse($tenant->updated_at)->format('M d Y') }}</td>
                           </tr>
                           @endif
                           <tr>
