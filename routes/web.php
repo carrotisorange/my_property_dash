ee59a4d3e30c20f5972e93d7d6bfbe159a22b2b9
unknown <landleydgreat@gmail.com>
2020-08-13 23:23:08 +0800

    asdas

--- routes/web.php
+++ routes/web.php
@@ -1094,7 +1094,7 @@ Route::get('login/google', 'Auth\LoginController@google');
 Route::get('login/google/callback', 'Auth\LoginController@googleCallback');
 
 
-Route::get('password/email', function(Request $request){
+Route::get('/password/email', function(Request $request){
     return $request->all();
 });
 
