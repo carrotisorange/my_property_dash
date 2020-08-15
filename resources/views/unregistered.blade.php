a5e1da56e671309ae621e690871ea7305da98b61
unknown <landleydgreat@gmail.com>
2020-08-15 13:48:18 +0800

    asdasd

--- resources/views/unregistered.blade.php
+++ resources/views/unregistered.blade.php
@@ -224,7 +224,7 @@
         <div class="text-center">
             <div class="error mx-auto" data-text="404">404</div>
             <p class="lead text-gray-800 mb-5">Page Not Found</p>
-            <p class="text-gray-500 mb-0">Please wait. Your account is being reviewed...</p>
+            {{-- <p class="text-gray-500 mb-0">Please wait. Your account is being ...</p> --}}
            
           </div>
 
