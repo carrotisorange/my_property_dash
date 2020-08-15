5af716c0cfcaf13873f24d1cf8506724a2d33c4a
unknown <landleydgreat@gmail.com>
2020-08-15 13:52:12 +0800

    asdasd

--- resources/views/account-payables/account-payables.blade.php
+++ resources/views/account-payables/account-payables.blade.php
@@ -346,7 +346,7 @@
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Godie Enterprises 2020</span>
+            <span>Copyright &copy; The PMO Co 2020</span>
           </div>
         </div>
       </footer>
