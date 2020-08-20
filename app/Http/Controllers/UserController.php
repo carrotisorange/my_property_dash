4df1af1f0a513b59f8235dbab14a865436c5c01d
unknown <landleydgreat@gmail.com>
2020-08-20 10:08:03 +0800

    asdasd

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -122,8 +122,6 @@ class UserController extends Controller
         ->whereIn('email', ['lenwilljoy_06@yahoo.com', 'shanegonzales@gmail.com', '	jmelg.0707@gmail.com', 'marthaoyonc@gmail.com', 'marthaleasingcourtyards@gmail.com', 'marthaleasingcy@yahoo.com', 'marthagoshenland@yahoo.com.ph','marthaleasingnc@yahoo.com', 'aldrinqm12@gmail.com'])
         ->update(
                 [
-                    'name' => $request->name,
-                    'email' => $request->email,
                     'property_type' => 'Apartment Rentals'
                 ]
             );
