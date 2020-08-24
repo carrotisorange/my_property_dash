cb13d17a34d7c81c5f27f6dc86d447f99a7e1544
unknown <landleydgreat@gmail.com>
2020-08-24 23:21:41 +0800

    asasd

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -29,11 +29,8 @@
  <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
   <!-- Sidebar Toggle (Topbar) -->
-  <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-    <i class="fa fa-bars"></i>
-  </button>
+   <!-- Nav Item - Dashboard -->
 
-   {{ Auth::user()->property.' '.Auth::user()->property_type }}
 
   <!-- Topbar Navbar -->
   <ul class="navbar-nav ml-auto">
--- resources/views/select-plan.blade.php
+++ resources/views/select-plan.blade.php
@@ -23,39 +23,37 @@
 
 <body class="bg-gradient-primary">
          <!-- Topbar -->
- <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+     <!-- Topbar -->
+     <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-  <!-- Sidebar Toggle (Topbar) -->
-  <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-    <i class="fa fa-bars"></i>
-  </button>
-
-   {{ Auth::user()->property.' '.Auth::user()->property_type }}
-
-  <!-- Topbar Navbar -->
-  <ul class="navbar-nav ml-auto">
-
-    <div class="topbar-divider d-none d-sm-block"></div>
-
-    <!-- Nav Item - User Information -->
-    <li class="nav-item dropdown no-arrow">
-      <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
-        <i class="fas fa-users-circle"></i> 
-      </a>
-      <!-- Dropdown - User Information -->
-      <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-
-        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-          <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-          Logout
-        </a>
-      </div>
-    </li>
-
-  </ul>
-
-</nav>
+      <!-- Sidebar Toggle (Topbar) -->
+       <!-- Nav Item - Dashboard -->
+    
+    
+      <!-- Topbar Navbar -->
+      <ul class="navbar-nav ml-auto">
+    
+        <div class="topbar-divider d-none d-sm-block"></div>
+    
+        <!-- Nav Item - User Information -->
+        <li class="nav-item dropdown no-arrow">
+          <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
+            <i class="fas fa-users-circle"></i> 
+          </a>
+          <!-- Dropdown - User Information -->
+          <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+    
+            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+              <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+              Logout
+            </a>
+          </div>
+        </li>
+    
+      </ul>
+    
+    </nav>
 
   <div class="container">
 
@@ -123,7 +121,7 @@
 
                 <hr>
                     <button form="selectingPlanForm" type="submit" class="btn btn-primary btn-user btn-block" id="registerButton" onclick="this.form.submit(); this.disabled = true;"> 
-                       Next
+                       Finish
                     </button>
     
               </div>
