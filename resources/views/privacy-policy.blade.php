9d9c41a23296408c9e9fcef1876125216f10d8e6
unknown <landleydgreat@gmail.com>
2020-08-27 16:55:20 +0800

    asdasd

--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
   
-    <title>The Property Manager</title>
+    <title>The Property Manager | Privacy Policy</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
   
