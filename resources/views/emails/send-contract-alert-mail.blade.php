61b95420bf9ad68b8d79d4727eda40d12581e707
unknown <landleydgreat@gmail.com>
2020-08-29 23:17:36 +0800

    asd

--- resources/views/emails/send-contract-alert-mail.blade.php
+++ resources/views/emails/send-contract-alert-mail.blade.php
@@ -3,9 +3,9 @@
 
     <p>Your contract in <b>{{ $unit }}</b> is set to expire on <b>{{ Carbon\Carbon::parse($contract_ends_at)->format('M d Y') }}</b>, exactly <b>{{ $days_before_moveout }} days </b> from now. </p> 
         
-    <p>Would you like to extend your contract, if yes, for how long? </p>
+    <p>Would you like to extend your contract?If yes, for how long? </p>
 
-    <p><b>This is a system generated message, and we do not receive emails from this account, please let us know your response atleast a week before your moveout date through this email {{ Auth::user()->email }} instead. </b></p>
+    <p><b>This is a system generated message, and we do not receive emails from this account. Please let us know your response atleast a week before your moveout date through this email {{ Auth::user()->email }} instead. </b></p>
 
     Sincerely,
     <br>
