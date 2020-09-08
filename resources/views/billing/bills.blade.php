dd216392bc8fe5e7f6e94a58f13b6e8192ad1a18
unknown <landleydgreat@gmail.com>
2020-09-08 14:29:01 +0800

    remove all the zeroes in bills.

--- resources/views/billing/bills.blade.php
+++ resources/views/billing/bills.blade.php
@@ -359,7 +359,7 @@
           @endforeach
 
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Bills</h1>
+            <h1 class="h3 mb-0 text-gray-800">Bills ({{ $bills->count() }})</h1>
             <div class="dropdown show">
               <br>
               <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-plus fa-sm text-white-50"></i> Add Bills</a>
