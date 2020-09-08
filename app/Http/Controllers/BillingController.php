fccd49a6b9b768ed27c820ac3ffdeee53d7adb47
unknown <landleydgreat@gmail.com>
2020-09-08 14:55:10 +0800

    remove all the zeroes in bills.

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -130,7 +130,7 @@ class BillingController extends Controller
      */
     public function destroy($tenant_id, $billing_id)
     {
-        DB::table('billings')->whereNull('billing_start')->whereNull('billing_end')->delete();
+        DB::table('billings')->where('billing_id', $billing_id)->delete();
         return back()->with('success', 'Bill has been deleted');
     }
 }
