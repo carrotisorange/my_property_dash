9ac8e82c2c2c7a27e2928328a4e47a89de5b3057
unknown <landleydgreat@gmail.com>
2020-09-10 11:30:24 +0800

    asda

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -152,7 +152,12 @@ class BillingController extends Controller
      */
     public function destroy($tenant_id, $billing_id)
     {
-        DB::table('billings')->where('billing_id', $billing_id)->delete();
-        return back()->with('success', 'Bill has been deleted');
+        DB::table("billings")
+        ->join('tenants', 'billing_tenant_id', 'tenant_id')
+        ->join('units', 'unit_tenant_id', 'unit_id')
+        ->where('billing_desc', 'Rent')
+        ->delete();
+        // DB::table('billings')->where('billing_id', $billing_id)->delete();
+        // return back()->with('success', 'Bill has been deleted');
     }
 }
