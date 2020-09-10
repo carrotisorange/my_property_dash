6c358cf2e51fbbba8bafa623717df6cc5483e713
unknown <landleydgreat@gmail.com>
2020-09-10 18:05:43 +0800

    asd

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -137,13 +137,13 @@ class BillingController extends Controller
     {
 
 
-        //    DB::table("billings")
-        // ->join('tenants', 'billing_tenant_id', 'tenant_id')
-        // ->join('units', 'unit_tenant_id', 'unit_id')
-        // ->where('unit_property', Auth::user()->property)
-        // ->delete();
+           DB::table("billings")
+        ->join('tenants', 'billing_tenant_id', 'tenant_id')
+        ->join('units', 'unit_tenant_id', 'unit_id')
+        ->where('unit_property', Auth::user()->property)
+        ->delete();
 
-        DB::table('billings')->where('billing_id', $billing_id)->delete();
-        return back()->with('success', 'Bill has been deleted');
+        // DB::table('billings')->where('billing_id', $billing_id)->delete();
+        // return back()->with('success', 'Bill has been deleted');
     }
 }
