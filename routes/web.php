81b67ddafb1810279ed2bcb6f84fc6a7ff0b9c8b
unknown <landleydgreat@gmail.com>
2020-09-16 11:33:24 +0800

    asd

--- routes/web.php
+++ routes/web.php
@@ -740,9 +740,9 @@ Route::get('/home', function(){
 
     if(auth()->user()->user_type === 'manager' || auth()->user()->user_type === 'admin' ){
 
-        $units_count = DB::table('units')
+        return $units_count = DB::table('units')
             ->where('unit_property', Auth::user()->property)
-            ->count();
+            ->get();
 
         $units = DB::table('units')
             ->where('unit_property', Auth::user()->property)
