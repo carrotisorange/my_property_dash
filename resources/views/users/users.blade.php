8904c5a92663f2897a7e964880bff8b55f86f890
unknown <landleydgreat@gmail.com>
2020-09-16 20:21:59 +0800

    asd

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -509,6 +509,7 @@
           </div>
 
         </div>
+
         <div class="row">
           <!-- Area Chart -->
           <div class="col-xl-12 col-lg-12">
@@ -538,6 +539,82 @@
             </div>
           </div>
                   </div>
+
+                                          <!-- Content Row -->
+                                          <div class="row">
+                  
+                                            <!-- Content Column -->
+                                            <div class="col-lg-12 mb-4">
+                                              <!-- DataTales Example -->
+                                              <div class="card shadow mb-4">
+                                                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                                  <h6 class="m-0 font-weight-bold text-primary">LOGINS ({{ $sessions->count() }})</h6>
+                                                  <div class="dropdown no-arrow">
+                                                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                      <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                                                    </a> 
+                                                     <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                                                      {{-- <div class="dropdown-header">Dropdown Header:</div> --}}
+                                                      <a class="dropdown-item" target="_blank" href="/logins">See All</a>
+                                                      {{-- <a class="dropdown-item" href="#">Another action</a>
+                                                      <div class="dropdown-divider"></div>
+                                                      <a class="dropdown-item" href="#">Something else here</a> --}}
+                                                    </div> 
+                                                  </div>
+                                                </div>
+                                           
+                                               <div class="card-body">
+                                                <div class="table-responsive text-nowrap">
+                                                   <table class="table table-striped" >
+                                                     <thead>
+                                                       <tr>
+                                                        <th>USER</th>
+                                                        <th>EMAIL</th>
+                                                        <th>ROLE</th>
+                                                        <th>PROPERTY</th>
+                                                        <th>LOGIN AT</th>
+                                                        <th>STATUS</th>
+                                                     </tr>
+                                                     </thead>
+                                                     <tbody>
+                                                      @foreach ($sessions as $item)
+                                                      <tr>
+                                                        <td><a href="/users/{{ $item->id }}">{{ $item->name }}</a></td>
+                                                        <td>{{ $item->email }}</td>
+                                                        <td>{{ $item->user_type }}</td>
+                                                        <td>{{ $item->property }}</td>
+                                                        <td>{{ Carbon\Carbon::parse($item->session_last_login_at)->toTimeString() }}</td>
+                                                        <?php  
+                                                                                      $diffInMinutes = Carbon\Carbon::parse($item->last_logout_at)->diffInMinutes();
+                                                                                      $diffInHours = Carbon\Carbon::parse($item->last_logout_at)->diffInHours();
+                                                                                      $diffInDays = Carbon\Carbon::parse($item->last_logout_at)->diffInDays()
+                                                                                   ?>
+                                                                                   <td>
+                                                                                      @if($item->user_current_status === 'online')
+                                                                                     <span class="badge badge-success"> {{ $item->user_current_status }}</span>
+                                                                                     @else
+                                                                                      @if($diffInMinutes < 60)
+                                                                                      <span class="badge badge-secondary"> {{ $diffInMinutes }} minutes ago</span> 
+                                                                                        @elseif($diffInHours > 24)
+                                                                                        <span class="badge badge-secondary"> {{ $diffInDays }} days ago</span>
+                                                                                        @else
+                                                                                        <span class="badge badge-secondary">  {{ $diffInHours }} hours ago</span>
+                                                                                        @endif
+                                                                                     @endif
+                                                                                    </td> 
+                                                      </tr>
+                                                      @endforeach
+                                                     </tbody>
+                                                   </table>
+                                                  
+                                                 </div>
+                                               </div>
+                                             </div>
+                                     
+                                                 </div>
+                          
+                                       
+                                          </div>
 
        
                         <!-- Content Row -->
@@ -728,81 +805,7 @@
                                                 </div>
         @endif
                        
-                        <!-- Content Row -->
-                        <div class="row">
-                  
-                          <!-- Content Column -->
-                          <div class="col-lg-12 mb-4">
-                            <!-- DataTales Example -->
-                            <div class="card shadow mb-4">
-                              <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                <h6 class="m-0 font-weight-bold text-primary">LOGINS ({{ $sessions->count() }})</h6>
-                                <div class="dropdown no-arrow">
-                                  <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                                  </a> 
-                                   <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                                    {{-- <div class="dropdown-header">Dropdown Header:</div> --}}
-                                    <a class="dropdown-item" target="_blank" href="/logins">See All</a>
-                                    {{-- <a class="dropdown-item" href="#">Another action</a>
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="#">Something else here</a> --}}
-                                  </div> 
-                                </div>
-                              </div>
-                         
-                             <div class="card-body">
-                              <div class="table-responsive text-nowrap">
-                                 <table class="table table-striped" >
-                                   <thead>
-                                     <tr>
-                                      <th>USER</th>
-                                      <th>EMAIL</th>
-                                      <th>ROLE</th>
-                                      <th>PROPERTY</th>
-                                      <th>LOGIN AT</th>
-                                      <th>STATUS</th>
-                                   </tr>
-                                   </thead>
-                                   <tbody>
-                                    @foreach ($sessions as $item)
-                                    <tr>
-                                      <td><a href="/users/{{ $item->id }}">{{ $item->name }}</a></td>
-                                      <td>{{ $item->email }}</td>
-                                      <td>{{ $item->user_type }}</td>
-                                      <td>{{ $item->property }}</td>
-                                      <td>{{ Carbon\Carbon::parse($item->session_last_login_at)->toTimeString() }}</td>
-                                      <?php  
-                                                                    $diffInMinutes = Carbon\Carbon::parse($item->last_logout_at)->diffInMinutes();
-                                                                    $diffInHours = Carbon\Carbon::parse($item->last_logout_at)->diffInHours();
-                                                                    $diffInDays = Carbon\Carbon::parse($item->last_logout_at)->diffInDays()
-                                                                 ?>
-                                                                 <td>
-                                                                    @if($item->user_current_status === 'online')
-                                                                   <span class="badge badge-success"> {{ $item->user_current_status }}</span>
-                                                                   @else
-                                                                    @if($diffInMinutes < 60)
-                                                                    <span class="badge badge-secondary"> {{ $diffInMinutes }} minutes ago</span> 
-                                                                      @elseif($diffInHours > 24)
-                                                                      <span class="badge badge-secondary"> {{ $diffInDays }} days ago</span>
-                                                                      @else
-                                                                      <span class="badge badge-secondary">  {{ $diffInHours }} hours ago</span>
-                                                                      @endif
-                                                                   @endif
-                                                                  </td> 
-                                    </tr>
-                                    @endforeach
-                                   </tbody>
-                                 </table>
-                                
-                               </div>
-                             </div>
-                           </div>
-                   
-                               </div>
-        
-                     
-                        </div>
+
       
 
                                                 <!-- Content Row -->
