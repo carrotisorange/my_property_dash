72e4d17f11017a167c956dc63ce31ccf6b644166
unknown <landleydgreat@gmail.com>
2020-09-25 15:42:50 +0800

    asd

--- resources/views/billing/edit-billings.blade.php
+++ resources/views/billing/edit-billings.blade.php
@@ -132,7 +132,7 @@
 
 @section('content')
 @if(Auth::user()->user_type === 'manager')
-<a href="/units/{{ $tenant->unit_tenant_id }}/tenants/{{ $tenant->tenant_id }}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-arrow-left fa-sm text-white-50"></i> Bills</a>
+<a href="/units/{{ $tenant->unit_tenant_id }}/tenants/{{ $tenant->tenant_id }}#nav-bills" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-arrow-left fa-sm text-white-50"></i> Bills</a>
 @else
 <a href="/units/{{ $tenant->unit_tenant_id }}/tenants/{{ $tenant->tenant_id }}/billings" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-arrow-left fa-sm text-white-50"></i> Bills</a>
 @endif
