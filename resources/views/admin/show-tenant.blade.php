2d0aebef0290c747e221627b8aaee82958087fff
unknown <landleydgreat@gmail.com>
2020-09-29 13:28:37 +0800

    asd

--- resources/views/admin/show-tenant.blade.php
+++ resources/views/admin/show-tenant.blade.php
@@ -596,7 +596,7 @@
                   <div class="modal-dialog modal-md" role="document">
                   <div class="modal-content">
                       <div class="modal-header">
-                      <h5 class="modal-title" id="exampleModalLabel">Enter Concern Information</h5>
+                      <h5 class="modal-title" id="exampleModalLabel">Add Concern</h5>
               
                       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                           <span aria-hidden="true">&times;</span>
