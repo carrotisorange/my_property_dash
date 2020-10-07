b5019a3384126ffd2c080faadd94fca74d644eee
unknown <landleydgreat@gmail.com>
2020-10-07 12:44:25 +0800

    added the chatbot

--- resources/views/layouts/arsha/template.blade.php
+++ resources/views/layouts/arsha/template.blade.php
@@ -51,32 +51,29 @@
 </head>
 
 <body>
-  <!-- Load Facebook SDK for JavaScript -->
-  <div id="fb-root"></div>
-  <script>
-    window.fbAsyncInit = function() {
-      FB.init({
-        xfbml            : true,
-        version          : 'v8.0'
-      });
-    };
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v8.0'
+    });
+  };
 
-    (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));</script>
+  (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
-  <!-- Your Chat Plugin code -->
-  <div class="fb-customerchat"
-    attribution=install_email
-    page_id="105134548031280"
-theme_color="#7646ff"
-logged_in_greeting="Hi! How can we help you?"
-logged_out_greeting="Hi! How can we help you?">
-  </div>
+<!-- Your Chat Plugin code -->
+<div class="fb-customerchat"
+  attribution=setup_tool
+  page_id="580584885947359">
+</div>
   @section('nav-bar')
   @show
   <!-- ======= Header ======= -->
