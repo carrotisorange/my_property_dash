94725bb14999730e09ab95750c57395e40d8422b
unknown <landleydgreat@gmail.com>
2020-10-08 12:55:33 +0800

    changed the login layout.

--- resources/views/billing/collections.blade.php
+++ resources/views/billing/collections.blade.php
@@ -152,7 +152,7 @@
 </form>
 </div>
 <div class="table-responsive text-nowrap">
-    <table class="table">
+    <table class="table table-bordered">
       @foreach ($collections as $day => $collection_list)
         <tr>
             <th colspan="12">{{ Carbon\Carbon::parse($day)->addDay()->format('M d Y') }} ({{ $collection_list->count() }})</th>
