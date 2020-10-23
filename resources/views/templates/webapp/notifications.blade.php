b2c2999a2834577edf567b5c2c58e9700e3e05de
carrotisorange <thepropertymanager20202@gmail.com>
2020-10-23 13:45:18 +0800

    as

--- resources/views/templates/webapp/notifications.blade.php
+++ resources/views/templates/webapp/notifications.blade.php
@@ -1,6 +1,6 @@
 @foreach (['danger', 'warning', 'success', 'info'] as $key)
 @if(Session::has($key))
-<div class="col-md-6 p-0 m-0 mx-auto">
+<div class="col-md-6 p-0 m-0 mx-auto text-center">
     <div class="alert alert-{{ $key }} alert-dismissable custom-{{ $key }}-box">
         <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
         @if($key === 'danger')
