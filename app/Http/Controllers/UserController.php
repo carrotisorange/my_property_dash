7b7fff6d31c598394db2c725d750b0912ff2fb7b
carrotisorange <thepropertymanager20202@gmail.com>
2020-10-27 11:33:48 +0800

    asd

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -441,8 +441,8 @@ class UserController extends Controller
        
          $users = DB::table('users_properties_relations')
         ->join('users', 'user_id_foreign', 'id')
-      
-        ->where('lower_access_user_id', Auth::user()->id)
+        ->where('property_id_foreign', $property_id)
+       
         ->get();
 
         $property = Property::findOrFail($property_id);
