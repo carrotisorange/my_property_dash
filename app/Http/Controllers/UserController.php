93622f1a9eb4f47f36883a866f1ef0fb60cc82b5
carrotisorange <thepropertymanager20202@gmail.com>
2020-10-27 11:23:49 +0800

    asd

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -439,7 +439,7 @@ class UserController extends Controller
 
     public function index_system_user($property_id){
        
-         $users = DB::table('users_properties_relations')
+       return  $users = DB::table('users_properties_relations')
         ->join('users', 'user_id_foreign', 'id')
         ->where('property_id_foreign', $property_id)
         ->orWhere('lower_access_user_id', Auth::user()->id)
