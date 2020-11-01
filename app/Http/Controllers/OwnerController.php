a6c7c47c2f65e158ccd8fd9eadc348cd2f666d73
carrotisorange <landleydgreat@gmail.com>
2020-11-01 22:40:39 +0800

    Update OwnerController.php

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -19,7 +19,7 @@ class OwnerController extends Controller
     {
         $owners = DB::table('units')
         ->join('unit_owners', 'unit_id', 'unit_id_foreign')
-        ->where('property_id_foreign', $property_id)
+      
         ->get();
 
         $count_owners = DB::table('units')
