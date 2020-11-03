9a1ffc0d031052bd18a83ce00b4f1adb8147de9c
carrotisorange <thepropertymanager20202@gmail.com>
2020-11-03 11:14:17 +0800

    asd

--- resources/views/webapp/properties/index.blade.php
+++ resources/views/webapp/properties/index.blade.php
@@ -22,8 +22,8 @@
       <div class="card-body">
         <div class="row">
           <div class="col">
-            <h5 class="card-title text-uppercase text-muted mb-0">{{ $item->name }}</h5>
-            <span class="h2 font-weight-bold mb-0">{{ $item->type}} &#9671 {{ $item->ownership }} </span>
+            <span class="h2 font-weight-bold mb-0">{{ $item->name }}</span>
+            <h5 class="card-title text-uppercase text-muted mb-0">{{ $item->type}} &#9671 {{ $item->ownership }} </h5>
           </div>
           <div class="col-auto">
             
